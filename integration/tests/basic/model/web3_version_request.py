97c606324435737cb43f42b9c31484c995dc1ea8
Alexander Petrovskiy <uiautomation@hotmail.com>
2022-02-23 09:41:09 +0300

    models

---
+++ integration/tests/basic/model/web3_version_request.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+from integration.tests.basic.model.rpc_request import RpcRequest
+
+
+@dataclass
+class Web3ClientVersion(RpcRequest):
+    id:int
+    result:str

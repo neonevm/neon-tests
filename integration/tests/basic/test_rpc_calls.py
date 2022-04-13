aea936239ccc7650c13215c973651b46b1f6916d
Eugeny Kurkovich <ek@neonlabs.org>
2022-04-13 15:35:36 +0300

    RPC calls: Fix naming

--- integration/tests/basic/test_rpc_calls.py
+++ integration/tests/basic/test_rpc_calls.py
@@ -56,7 +56,7 @@ TAGS_TEST_DATA = [
 class TestRpcCalls(BaseMixin):
     # TODO: implement numerous variants
 
-    def test_rpc_call_eth_call(self, prepare_accounts):
+    def test_eth_call(self, prepare_accounts):
         """Verify implemented rpc calls work eth_call"""
 
         self.transfer_neon(self.sender_account, self.recipient_account, InputData.SAMPLE_AMOUNT.value)
@@ -75,7 +75,7 @@ class TestRpcCalls(BaseMixin):
 
     # TODO: implement numerous variants
 
-    def test_rpc_call_eth_estimateGas(self, prepare_accounts):
+    def test_eth_estimate_gas(self, prepare_accounts):
         """Verify implemented rpc calls work eth_estimateGas"""
 
         # TOOD: variants
@@ -90,7 +90,7 @@ class TestRpcCalls(BaseMixin):
         # assert self.assert_result_object(
         #     actual_result), AssertMessage.DOES_NOT_CONTAIN_RESULT
 
-    def test_rpc_call_eth_gasPrice(self):
+    def test_eth_gas_price(self):
         """Verify implemented rpc calls work eth_gasPrice"""
         model = RpcRequestFactory.get_gas_price(params=[])
         actual_result = self.json_rpc_client.do_call(model)
@@ -100,7 +100,7 @@ class TestRpcCalls(BaseMixin):
         assert "0x" in actual_result.result, AssertMessage.DOES_NOT_START_WITH_0X.value
 
     @pytest.mark.parametrize("from_block,to_block", GET_LOGS_TEST_DATA)
-    def test_rpc_call_eth_getLogs_via_tags(self, from_block: Tag, to_block: Tag):
+    def test_eth_get_logs_via_tags(self, from_block: Tag, to_block: Tag):
         """Verify implemented rpc calls work eth_getLogs"""
         # TODO: use contract instead of account
         sender_account = self.create_account_with_balance()
@@ -113,7 +113,7 @@ class TestRpcCalls(BaseMixin):
         assert self.assert_no_error_object(actual_result), AssertMessage.CONTAINS_ERROR
         assert self.assert_result_object(actual_result), AssertMessage.DOES_NOT_CONTAIN_RESULT
 
-    def test_rpc_call_eth_getLogs_via_numbers(self):
+    def test_eth_get_logs_via_numbers(self):
         """Verify implemented rpc calls work eth_getLogs"""
         # TODO: use contract instead of account
         sender_account = self.create_account_with_balance()
@@ -128,7 +128,7 @@ class TestRpcCalls(BaseMixin):
         assert self.assert_result_object(actual_result), AssertMessage.DOES_NOT_CONTAIN_RESULT
 
     @pytest.mark.only_stands
-    def test_rpc_call_eth_getBalance(self):
+    def test_eth_get_balance(self):
         """Verify implemented rpc calls work eth_getBalance"""
         sender_account = self.create_account_with_balance()
 
@@ -139,7 +139,7 @@ class TestRpcCalls(BaseMixin):
         assert actual_result.id == model.id, AssertMessage.WRONG_ID.value
         assert actual_result.result == InputData.FIRST_AMOUNT_IN_RESPONSE.value, AssertMessage.WRONG_AMOUNT.value
 
-    def test_rpc_call_eth_getCode(self):
+    def test_eth_get_code(self):
         """Verify implemented rpc calls work eth_getCode"""
         # TODO: use contract instead of account?
         sender_account = self.create_account_with_balance()
@@ -152,12 +152,7 @@ class TestRpcCalls(BaseMixin):
         assert self.assert_no_error_object(actual_result), AssertMessage.CONTAINS_ERROR
         assert self.assert_result_object(actual_result), AssertMessage.DOES_NOT_CONTAIN_RESULT
 
-    @pytest.mark.skip(WAITIING_FOR_CONTRACT_SUPPORT)
-    def test_rpc_call_eht_getStorageAt(self):
-        """Verify implemented rpc calls work eht_getStorageAt"""
-        pass
-
-    def test_rpc_call_web3_clientVersion(self):
+    def test_web3_client_version(self):
         """Verify implemented rpc calls work web3_clientVersion"""
         model = RpcRequestFactory.get_web3_client_version(params=[])
         actual_result = self.json_rpc_client.do_call(model)
@@ -166,7 +161,7 @@ class TestRpcCalls(BaseMixin):
         assert self.assert_is_successful_response(actual_result), AssertMessage.WRONG_TYPE.value
         assert "Neon" in actual_result.result, "version does not contain 'Neon'"
 
-    def test_rpc_call_net_version(self):
+    def test_net_version(self):
         """Verify implemented rpc calls work work net_version"""
         model = RpcRequestFactory.get_net_version(params=[])
         actual_result = self.json_rpc_client.do_call(model)
@@ -177,7 +172,7 @@ class TestRpcCalls(BaseMixin):
             self.web3_client._chain_id
         ), f"net version is not {self.web3_client._chain_id}"
 
-    def test_rpc_call_eth_getBlockByHash(self, prepare_accounts):
+    def test_eth_get_block_by_hash(self, prepare_accounts):
         """Verify implemented rpc calls work eth_getBlockByHash"""
 
         tx_receipt = self.transfer_neon(self.sender_account, self.recipient_account, InputData.SAMPLE_AMOUNT.value)
@@ -192,7 +187,7 @@ class TestRpcCalls(BaseMixin):
         assert self.assert_result_object(actual_result), AssertMessage.DOES_NOT_CONTAIN_RESULT
 
     @pytest.mark.parametrize("quantity_tag,full_trx", TAGS_TEST_DATA)
-    def test_rpc_call_eth_getBlockByNumber_via_tags(self, quantity_tag: Union[int, Tag], full_trx: bool):
+    def test_eth_get_blockn_by_umber_via_tags(self, quantity_tag: Union[int, Tag], full_trx: bool):
         """Verify implemented rpc calls work eth_getBlockByNumber"""
         params = RpcRequestParamsFactory.get_block_by_number(quantity_tag, full_trx)
         model = RpcRequestFactory.get_block_by_number(params=params)
@@ -200,7 +195,7 @@ class TestRpcCalls(BaseMixin):
         actual_result = self.json_rpc_client.do_call(model, BlockResponse)
         assert actual_result.id == model.id, AssertMessage.WRONG_ID.value
 
-    def test_rpc_call_eth_getBlockByNumber_via_numbers(self, prepare_accounts):
+    def test_eth_get_block_by_number_via_numbers(self, prepare_accounts):
         """Verify implemented rpc calls work eth_getBlockByNumber"""
 
         tx_receipt = self.transfer_neon(self.sender_account, self.recipient_account, InputData.SAMPLE_AMOUNT.value)
@@ -213,7 +208,7 @@ class TestRpcCalls(BaseMixin):
         assert self.assert_no_error_object(actual_result), AssertMessage.CONTAINS_ERROR
         assert self.assert_result_object(actual_result), AssertMessage.DOES_NOT_CONTAIN_RESULT
 
-    def test_rpc_call_eth_blockNumber(self):
+    def test_eth_block_number(self):
         """Verify implemented rpc calls work work eth_blockNumber"""
         model = RpcRequestFactory.get_block_number(params=[])
         actual_result = self.json_rpc_client.do_call(model)
@@ -221,3 +216,8 @@ class TestRpcCalls(BaseMixin):
         assert actual_result.id == model.id, AssertMessage.WRONG_ID.value
         assert self.assert_is_successful_response(actual_result), AssertMessage.WRONG_TYPE.value
         assert "0x" in actual_result.result, AssertMessage.DOES_NOT_START_WITH_0X.value
+
+    @pytest.mark.skip(WAITIING_FOR_CONTRACT_SUPPORT)
+    def test_eht_get_storage_at(self):
+        """Verify implemented rpc calls work eht_getStorageAt"""
+        pass

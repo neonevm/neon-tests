b6e6762c9813e3bfb80c7f2a539b3b3f0766c9a8
gigimon <gigimon4ik@gmail.com>
2022-04-18 15:53:02 +0300

    * reusable allure report from openzeppelin, upload it to s3 storage

--- clickfile.py
+++ clickfile.py
@@ -5,15 +5,21 @@ import glob
 import json
 import shutil
 import sys
-import click
 import subprocess
 import pathlib
 from multiprocessing.dummy import Pool
 from typing import Dict, Tuple, List
 
 try:
+    import click
+except ImportError:
+    print("Please install click library: pip install click==8.0.3")
+    sys.exit(1)
+
+try:
     from utils import web3client
     from utils import faucet
+    from utils import cloud
 except ImportError:
     pass
 
@@ -61,7 +67,7 @@ def run_openzeppelin_tests(network, jobs=8):
         print(stdout)
         print(stderr)
         keys_env.append(keys)
-        log_dirs = (cwd.parent / "results" / file_name.replace(".", "_").replace('/', '_'))
+        log_dirs = cwd.parent / "results" / file_name.replace(".", "_").replace("/", "_")
         log_dirs.mkdir(parents=True, exist_ok=True)
         with open(log_dirs / "stdout.log", "w") as f:
             f.write(stdout)
@@ -78,33 +84,26 @@ def run_openzeppelin_tests(network, jobs=8):
     opts = {
         "Proxy.Version": web3_client.get_proxy_version()["result"],
         "EVM.Version": web3_client.get_evm_version()["result"],
-        "CLI.Version": web3_client.get_cli_version()["result"]
+        "CLI.Version": web3_client.get_cli_version()["result"],
     }
     with open("./allure-results/environment.properties", "w+") as f:
         f.write("\n".join(map(lambda x: f"{x[0]}={x[1]}", opts.items())))
         f.write("\n")
     # Add epic name for allure result files
-    openzeppelin_reports = pathlib.Path('./allure-results')
-    res_file_list = [str(res_file) for res_file in openzeppelin_reports.glob('*-result.json')]
+    openzeppelin_reports = pathlib.Path("./allure-results")
+    res_file_list = [str(res_file) for res_file in openzeppelin_reports.glob("*-result.json")]
     print("Fix allure results: {}".format(len(res_file_list)))
 
     for res_file in res_file_list:
-        with open(res_file, 'r+') as f:
+        with open(res_file, "r+") as f:
             report = json.load(f)
-        report["labels"].append({
-            "name": "epic",
-            "value": "OpenZeppelin contracts"
-        })
-        with open(res_file, 'w+') as f:
+        report["labels"].append({"name": "epic", "value": "OpenZeppelin contracts"})
+        with open(res_file, "w+") as f:
             json.dump(report, f)
 
 
 def parse_openzeppelin_results():
-    test_report = {
-        "passing": 0,
-        "pending": 0,
-        "failing": 0
-    }
+    test_report = {"passing": 0, "pending": 0, "failing": 0}
 
     skipped_files = []
 
@@ -112,7 +111,7 @@ def parse_openzeppelin_results():
     print("`stdout` files found: {}. Processing ...\n".format(len(stdout_files)))
 
     for stdout in stdout_files:
-        with open(stdout, 'r+', encoding="utf8") as f:
+        with open(stdout, "r+", encoding="utf8") as f:
             rep = f.read()
             result = re.findall(r"(\d+) (passing|pending|failing)", rep)
             if not result:
@@ -120,6 +119,7 @@ def parse_openzeppelin_results():
             for count in result:
                 test_report[count[1]] += int(count[0])
     return test_report, skipped_files
+
 
 def print_test_suite_results(test_report: Dict[str, int], skipped_files: List[str]):
     print("Summarize result:\n")
@@ -165,8 +165,9 @@ def requirements():
 
 
 @cli.command(help="Run any type of tests")
-@click.option("-n", "--network", default="n ight-stand", type=click.Choice(networks.keys()),
-              help="In which stand run tests")
+@click.option(
+    "-n", "--network", default="n ight-stand", type=click.Choice(networks.keys()), help="In which stand run tests"
+)
 @click.option("-j", "--jobs", default=8, help="Number of parallel jobs (for openzeppelin)")
 @click.argument("name", required=True, type=click.Choice(["economy", "basic", "oz"]))
 def run(name, network, jobs):
@@ -230,15 +231,14 @@ def ozreport():
     "--run-time",
     type=int,
     help="Stop after the specified amount of time, e.g. (300s, 20m, 3h, 1h30m, etc.). "
-         "Only used together without Locust Web UI. [default: always run]",
+    "Only used together without Locust Web UI. [default: always run]",
 )
 @click.option(
     "-T",
     "--tag",
     type=str,
     multiple=True,
-    help="tag to include in the test, so only tasks "
-         "with any matching tags will be executed"
+    help="tag to include in the test, so only tasks " "with any matching tags will be executed",
 )
 @click.option(
     "--web-ui/--headless",
@@ -268,6 +268,45 @@ def locust(locustfile, credentials, host, users, spawn_rate, run_time, tag, web_
 
     if cmd.returncode != 0:
         sys.exit(cmd.returncode)
+
+
+@cli.command(help="Download allure history")
+@click.argument("name", type=click.STRING)
+@click.option(
+    "-n", "--network", default="night-stand", type=click.Choice(networks.keys()), help="In which stand run tests"
+)
+@click.option("-d", "--destination", default="./allure-results", type=click.Path(file_okay=False, dir_okay=True))
+def get_allure_history(name: str, network: str, destination: str = "./allure-results"):
+    branch = os.environ.get("GITHUB_REF_NAME")
+    path = pathlib.Path(name) / network / branch
+
+    runs = []
+    previous_runs = cloud.list_bucket(path)
+    for run in previous_runs:
+        if run.get("Key").isdigit():
+            runs.append(int(run))
+    if len(runs) > 0:
+        cloud.download(path / str(max(runs)) / "history", pathlib.Path(destination) / "history")
+
+
+@cli.command(help="Upload allure report")
+@click.argument("name", type=click.STRING)
+@click.option(
+    "-n", "--network", default="night-stand", type=click.Choice(networks.keys()), help="In which stand run tests"
+)
+@click.option("-s", "--source", default="./allure-report", type=click.Path(file_okay=False, dir_okay=True))
+def upload_allure_report(name: str, network: str, source: str = "./allure-report"):
+    branch = os.environ.get("GITHUB_REF_NAME")
+    build_id = os.environ.get("GITHUB_RUN_NUMBER")
+    path = pathlib.Path(name) / network / branch
+    cloud.upload(source, path / build_id)
+    with open("/tmp/index.html", "w") as f:
+        f.write(
+            f"""<!DOCTYPE html><meta charset="utf-8"><meta http-equiv="refresh" content="0; URL=http://neon-test-allure.s3-website.eu-central-1.amazonaws.com/{path / build_id}">
+        <meta http-equiv="Pragma" content="no-cache"><meta http-equiv="Expires" content="0">
+        """
+        )
+    cloud.upload("/tmp/index.html", path / "index.html")
 
 
 if __name__ == "__main__":

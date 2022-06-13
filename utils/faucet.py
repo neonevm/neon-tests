86b4fe3bd0dcc8b15982376ccd8520968dcbc6ad
gigimon <gigimon4ik@gmail.com>
2022-06-13 18:07:26 +0300

    * remove useless req

--- utils/faucet.py
+++ utils/faucet.py
@@ -1,10 +1,6 @@
-import time
 import requests
 import typing as tp
 import urllib.parse
-from busypie import FIVE_SECONDS, wait_at_most
-from busypie.durations import TWO_MINUTES
-from http import HTTPStatus
 
 
 class Faucet:
@@ -15,12 +11,6 @@ class Faucet:
     def request_neon(self, address: str, amount: int = 100):
         assert address.startswith("0x")
         url = urllib.parse.urljoin(self._url, "request_neon")
-        wait_at_most(TWO_MINUTES).poll_interval(FIVE_SECONDS).until(
-            lambda: self.send_post_request(url, address, amount)
-        )
-        # needed for devnet
-        time.sleep(1)
-
-    def send_post_request(self, url: str, address: str, amount: int) -> bool:
         response = self._session.post(url, json={"amount": amount, "wallet": address})
-        return True if HTTPStatus.OK == response.status_code else False
+        assert response.ok, "Faucet returned error: {}".format(response.text)
+        return response

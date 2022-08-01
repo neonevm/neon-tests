6c6a8989ffce58dd13c0412a5d1fcf3d6056af39
Eugeny Kurkovich <ek@neonlabs.org>
2022-08-01 16:33:42 +0300

    NDEV-368: fix push-readme.py

--- deploy/oz/push-readme.py
+++ deploy/oz/push-readme.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 
-import requests
-import os
-from requests.structures import CaseInsensitiveDict
 import logging
+import os
+import pathlib
 import sys
 
+import requests
+from requests.structures import CaseInsensitiveDict
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+README = "DOCKERHUB.md"
 
 try:
     logger.debug("Getting DHUBU, DHUBP from environment")
@@ -24,7 +27,7 @@ try:
     if username is None or password is None:
         logger.error("Failed to get username and password from environment")
         exit(1)
-    url = 'https://hub.docker.com/v2/users/login'
+    url = "https://hub.docker.com/v2/users/login"
     logger.debug("Request (post) JWT token from: " + url)
     response = requests.post(url, json={"username": username, "password": password})
     if response.status_code != 200:
@@ -36,8 +39,8 @@ try:
     headers["Accept"] = "application/json"
     headers["Authorization"] = "Bearer " + token
     headers["Content-Type"] = "application/json"
-    logger.debug("Open DOCKERHUB.md")
-    with open("DOCKERHUB.md") as readme_file:
+    logger.debug(f"Open {README}")
+    with open(pathlib.path(__file__).parent / README) as readme_file:
         readme_data = readme_file.read()
         url = "https://hub.docker.com/v2/repositories/neonlabsorg/full_test_suite/"
         logger.debug("Request (patch) to update full_description at " + url)

7ee4bc389bbff479c3e803f8f933a554d2bb4663
Eugeny Kurkovich <ek@neonlabs.org>
2022-08-24 18:43:46 +0300

    NDEV-342: Added neon-cli wrapper

---
+++ deploy/infra/devbox/.bash_history
@@ -0,0 +1,17 @@
+neon-cli
+pytest
+pytest -sx ./integration/tests/basic/test_rpc_calls.py 
+pip3 install -U web3
+pytest -sx ./integration/tests/basic/test_rpc_calls.py 
+git 
+git st
+ipython
+ipython
+neon-cli  -vvv --commitment=processed --url http://proxy.night.stand.neontest.xyz/node-solana --evm_loader 53DfF883gyixYNXnM7s5xhdeyV8mVk9T4i2hGV9vG9io create-program-address 17251B1E65f81bcd8b07E0Ffac2B82cF09E89b9a
+neon-cli --help
+neon-cli create-program-address --help
+neon-cli --help
+neon-cli  -vvv --config=/neon-tests/loadtesting/synthetic/operator-keypairs/id.json --commitment=processed --url http://proxy.night.stand.neontest.xyz/node-solana --evm_loader 53DfF883gyixYNXnM7s5xhdeyV8mVk9T4i2hGV9vG9io create-program-address 17251B1E65f81bcd8b07E0Ffac2B82cF09E89b9a
+less /neon-tests/loadtesting/synthetic/operator-keypairs/id.json
+neon-cli  -vvv --config=/neon-tests/loadtesting/synthetic/operator-keypairs/id.json --commitment=processed --url=http://proxy.night.stand.neontest.xyz/node-solana --evm_loader=53DfF883gyixYNXnM7s5xhdeyV8mVk9T4i2hGV9vG9io create-program-address 17251B1E65f81bcd8b07E0Ffac2B82cF09E89b9a
+neon-cli  -vvv --config /neon-tests/loadtesting/synthetic/operator-keypairs/id.json --commitment processed --url http://proxy.night.stand.neontest.xyz/node-solana --evm_loader 53DfF883gyixYNXnM7s5xhdeyV8mVk9T4i2hGV9vG9io create-program-address 17251B1E65f81bcd8b07E0Ffac2B82cF09E89b9a
--- loadtesting/synthetic/locustfile.py
+++ loadtesting/synthetic/locustfile.py
@@ -8,10 +8,12 @@ import json
 import logging
 import os
 import pathlib
+import subprocess
 import typing as tp
 
 import requests
 import web3
+from solana.account import Account as SOLAccount
 from solana.rpc.api import Client as solana_api_client
 
 from utils import faucet
@@ -34,6 +36,12 @@ DEFAULT_NEON_AMOUNT = 100
 """
 """
 
+CWD = pathlib.Path(__file__).parent
+"""Current working directory"""
+
+BASE_PATH = CWD.parent.parent
+"""Project root directory"""
+
 
 def init_session(size: int) -> requests.Session:
     """init request session with extended connection pool size"""
@@ -46,11 +54,84 @@ def init_session(size: int) -> requests.Session:
 
 def load_credentials(*args, **kwargs) -> tp.Dict:
     """Test start event handler"""
-    base_path = pathlib.Path(__file__).parent.parent.parent
-    path = base_path / ENV_FILE
+    path = BASE_PATH / ENV_FILE
     with open(path, "r") as fp:
         f = json.load(fp)
         return f[DEFAULT_NETWORK]
+
+
+class NeonCli:
+    """Implements neon client functionality"""
+
+    def __init__(self, evm_loader_id: str, solana_url: str, verbose_flags: tp.Optional[str] = "") -> None:
+        self._verbose_flags = verbose_flags
+        self._loader_id = evm_loader_id
+        self._solana_url = solana_url
+
+    def __getattr__(self, item):
+
+        global command
+        command = item
+
+        def wrapper(*args, **kwargs):
+            return self._run_cli(command, *args, **kwargs)
+
+        return wrapper
+
+    def _run_cli(self, comand, *args):
+        cmd = (
+            f"neon-cli {self._verbose_flags} "
+            f"-vvv "
+            f"--commitment=processed "
+            f"--url {self._solana_url} "
+            f"--evm_loader {self._loader_id} "
+            f"{comand.replace('_','-')} {''.join(map(str, args))}"
+        )
+        try:
+            return subprocess.check_output(cmd, shell=True, universal_newlines=True)
+        except subprocess.CalledProcessError as err:
+            print(f"ERR: neon-cli error {err}")
+            raise
+
+
+class OperatorAccount:
+    """Implements operator Account"""
+
+    def __init__(self, path=None) -> None:
+        self._path = path or CWD / "operator-keypairs/id.json"
+        self.account = self._create_account()
+
+    def _create_account(self) -> "solana.account.Account":
+        """Create an operator Account"""
+        with open(self.path) as fd:
+            key = json.load(fd)
+            return SOLAccount(key[0:32])
+
+    def get_path(self) -> str:
+        """Return operator key storage path"""
+        return self._path.as_posix()
+
+
+class EvmLoader:
+    """Implements base functionality of the evm loader"""
+
+    def __init__(self, loader_id: str, solana_url: str, account: tp.Optional["OperatorAccount"] = None) -> None:
+        self.loader_id = loader_id
+        self.account = account
+        self._neon_cli = NeonCli(self.loader_id, solana_url)
+
+    def ether2program(self, ether):
+        """Create solana program from eth account"""
+
+        if hasattr(ether, "address"):
+            ether = ether.address
+        elif not isinstance(ether, str):
+            ether = ether.hex()
+        if ether.startswith("0x"):
+            ether = ether[2:]
+        cli_output = self._neon_cli.create_program_address(ether)
+        items = cli_output.rstrip().split(' ')
+        return items[0], int(items[1])
 
 
 class SOLClient:
@@ -60,11 +141,15 @@ class SOLClient:
         self._session = session or init_session(DEFAULT_USER_NUM)
         self._faucet = faucet.Faucet(credentials["faucet_url"], self._session)
         self._solana = solana_api_client(credentials["solana_url"])
+        self._evm_loader = EvmLoader(credentials["evm_loader"], credentials["solana_url"])
 
-    def create_account(self) -> "eth_account.local.LocalAccount":
+    def create_eth_account(self) -> "eth_account.local.LocalAccount":
         account = self._web3.eth.account.create()
         self._faucet.request_neon(account.address, amount=DEFAULT_NEON_AMOUNT)
         return account
+
+    def create_solana_program(self, account: "eth_account.local.LocalAccount") -> tp.Tuple[tp.Any]:
+        return self._evm_loader.ether2program(account)
 
     def get_sol_balance(self, address: tp.Union[str, "eth_account.signers.local.LocalAccount"]) -> int:
         if not isinstance(address, str):

4a5003f0abd7b8131d5a31eb868a81929ce44249
Eugeny Kurkovich <ek@neonlabs.org>
2022-08-29 11:39:16 +0300

    NDEV-342: Fix SOL client

--- loadtesting/synthetic/locustfile.py
+++ loadtesting/synthetic/locustfile.py
@@ -3,6 +3,7 @@
 Created on 2022-08-18
 @author: Eugeny Kurkovich
 """
+import base64
 import functools
 import json
 import logging
@@ -16,9 +17,15 @@ from dataclasses import dataclass
 import requests
 import web3
 from solana.account import Account as SOLAccount
+from solana.keypair import Keypair
 from solana.publickey import PublicKey
 from solana.rpc import commitment
-from solana.rpc.api import Client as solana_client
+from solana.rpc.api import Client as SolanaClient
+
+# from solana.transaction import AccountMeta, TransactionInstruction  # , Transaction
+from eth_keys import keys as eth_keys
+from solana.rpc.providers import http
+from construct import Bytes, Int8ul, Struct
 
 from ui.libs import try_until
 from utils import faucet
@@ -47,6 +54,33 @@ CWD = pathlib.Path(__file__).parent
 
 BASE_PATH = CWD.parent.parent
 """Project root directory"""
+
+SYS_INSTRUCT_ADDRESS = "Sysvar1nstructions1111111111111111111111111"
+"""
+"""
+
+ACCOUNT_INFO_LAYOUT = Struct(
+    "type" / Int8ul,
+    "ether" / Bytes(20),
+    "nonce" / Int8ul,
+    "trx_count" / Bytes(8),
+    "balance" / Bytes(32),
+    "code_account" / Bytes(32),
+    "is_rw_blocked" / Int8ul,
+    "ro_blocked_cnt" / Int8ul,
+)
+
+
+@dataclass
+class AccountInfo:
+    ether: eth_keys.PublicKey
+    code_account: PublicKey
+    trx_count: int
+
+    @staticmethod
+    def from_bytes(data: bytes):
+        cont = ACCOUNT_INFO_LAYOUT.parse(data)
+        return AccountInfo(cont.ether, PublicKey(cont.code_account), cont.trx_count)
 
 
 @dataclass
@@ -88,6 +122,13 @@ def handle_failed_requests(func: tp.Callable) -> tp.Callable:
         return resp["result"]
 
     return wrapper
+
+
+class ExtendedHTTPProvider(http.HTTPProvider):
+    @handle_failed_requests
+    def make_request(self, *args, **kwargs) -> tp.Dict:
+        print(f"{30*'_'} Make extended request")
+        return super(ExtendedHTTPProvider, self).make_request(*args, **kwargs)
 
 
 class NeonClient:
@@ -162,26 +203,27 @@ class EvmLoader:
         return items[0], int(items[1])
 
 
-class SOLClient:
+class SOLClient(SolanaClient):
     """"""
 
-    def __init__(self, credentials: tp.Dict = None, session: tp.Optional[tp.Any] = None) -> None:
+    def __init__(self, credentials: tp.Dict = None, session: tp.Optional[tp.Any] = None, timeout: float = 10) -> None:
         credentials = credentials or load_credentials()
         self._web3 = web3.Web3(web3.HTTPProvider(""))
         self._session = session or init_session(DEFAULT_USER_NUM)
         self._faucet = faucet.Faucet(credentials["faucet_url"], self._session)
-        self._client = solana_client(credentials["solana_url"])
         self._evm_loader = EvmLoader(credentials["evm_loader"], credentials["solana_url"])
+        super(SOLClient, self).__init__()
+        self._provider = ExtendedHTTPProvider(credentials["solana_url"], timeout=timeout)
 
     def wait_confirmation(self, tx_sig: str, confirmations: tp.Optional[int] = 0) -> bool:
         """"""
 
         def get_signature_status():
-            resp = self._client.get_signature_statuses([tx_sig])
-            result = resp.get("result", {}).get("value")
-            if result[0]:
-                confirmation_status = result[0]["confirmationStatus"]
-                confirmation_count = result[0]["confirmations"] or 0
+            resp = self.get_signature_statuses([tx_sig])
+            result = resp.get("value", [None])[0]
+            if result:
+                confirmation_status = result["confirmationStatus"]
+                confirmation_count = result["confirmations"] or 0
                 return (
                     confirmation_status == SOLCommitmentState.FINALIZED
                     or confirmation_status == SOLCommitmentState.CONFIRMED
@@ -198,7 +240,6 @@ class SOLClient:
     def create_solana_program(self, account: "eth_account.local.LocalAccount") -> tp.Tuple[tp.Any]:
         return self._evm_loader.ether2program(account)
 
-    @handle_failed_requests
     def get_sol_balance(
         self,
         address: tp.Union[str, "eth_account.signers.local.LocalAccount"],
@@ -208,10 +249,9 @@ class SOLClient:
             address = str(address)
         elif not isinstance(address, str):
             address = address.address
-        return self._client.get_balance(address, commitment=state)
+        return self.get_balance(address, commitment=state)
 
-    @handle_failed_requests
-    def request_airdrop(
+    def request_sol(
         self,
         address: tp.Union[OperatorAccount, str],
         amount: int = DEFAULT_SOL_AMOUNT,
@@ -222,7 +262,75 @@ class SOLClient:
             address = address.public_key()
         elif isinstance(address, PublicKey):
             address = str(address)
-        return self._client.request_airdrop(pubkey=address, lamports=amount, commitment=state)
+        return self.request_airdrop(pubkey=address, lamports=amount, commitment=state)
+
+    def get_account_data(
+        self,
+        account: tp.Union[str, PublicKey, Keypair, OperatorAccount],
+        expected_length: int = ACCOUNT_INFO_LAYOUT.sizeof(),
+    ) -> bytes:
+        if isinstance(account, Keypair):
+            account = account.public_key
+        elif isinstance(account, OperatorAccount):
+            account = account.public_key()
+        resp = self.get_account_info(account, commitment=SOLCommitmentState.CONFIRMED).get("value")
+        if not resp:
+            raise Exception(f"Can't get information about {account}")
+        data = base64.b64decode(resp["data"][0])
+        if len(data) < expected_length:
+            raise Exception(f"Wrong data length for account data {account}")
+        return data
+
+    def get_transaction_count(self, account: tp.Union[str, PublicKey, OperatorAccount]) -> int:
+        if isinstance(account, Keypair):
+            account = account.public_key
+        elif isinstance(account, OperatorAccount):
+            account = account.public_key()
+        info = AccountInfo.from_bytes(self.get_account_data(account))
+        return int.from_bytes(info.trx_count, "little")
+
+    """
+    def make_PartialCallOrContinueFromRawEthereumTX(
+        self,
+        instruction: bytes,
+        operator: Keypair,
+        evm_loader: "EvmLoader",
+        storage_address: PublicKey,
+        treasury_address: PublicKey,
+        treasury_buffer: bytes,
+        step_count: int,
+        additional_accounts: tp.List[PublicKey],
+    ):
+        code = 13
+        d = code.to_bytes(1, "little") + treasury_buffer + step_count.to_bytes(8, byteorder="little") + instruction
+        operator_ether = eth_keys.PrivateKey(operator.secret_key[:32]).public_key.to_canonical_address()
+
+        accounts = [
+            AccountMeta(pubkey=storage_address, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=SYS_INSTRUCT_ADDRESS, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=operator.public_key, is_signer=True, is_writable=True),
+            AccountMeta(pubkey=treasury_address, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=evm_loader.ether2program(operator_ether)[0], is_signer=False, is_writable=True),
+            AccountMeta(SYS_PROGRAM_ID, is_signer=False, is_writable=True),
+            # Neon EVM account
+            AccountMeta(EVM_LOADER, is_signer=False, is_writable=False),
+        ]
+        for acc in additional_accounts:
+            accounts.append(
+                AccountMeta(acc, is_signer=False, is_writable=True),
+            )
+
+        return TransactionInstruction(program_id=EVM_LOADER, data=d, keys=accounts)
+
+    def make_eth_transaction(to_addr: str, data: bytes, signer: Keypair, from_solana_user: PublicKey,
+                             user_eth_address: bytes):
+        nonce = get_transaction_count(solana_client, from_solana_user)
+        tx = {'to': to_addr, 'value': 0, 'gas': 9999999999, 'gasPrice': 0,
+              'nonce': nonce, 'data': data, 'chainId': 111}
+        (from_addr, sign, msg) = make_instruction_data_from_tx(tx, signer.secret_key[:32])
+        assert from_addr == user_eth_address, (from_addr, user_eth_address)
+        return from_addr, sign, msg, nonce
+    """
 
 
 sol_client = SOLClient()

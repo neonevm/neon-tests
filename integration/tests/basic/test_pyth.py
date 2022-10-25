437a6a15ec3e0a4d0daf1e092d72ab6895a093a3
romanova-natasha <zabelina.nattaly@gmail.com>
2022-10-25 18:53:32 +0300

    NDEV-494: rm deploy QueryAccount contract
    NDEV-494: rm deploy QueryAccount contract

--- integration/tests/basic/test_pyth.py
+++ integration/tests/basic/test_pyth.py
@@ -15,9 +15,7 @@ class TestPyth(BaseTests):
 
     @pytest.mark.only_devnet
     def test_deploy_contract_pyth_network(self):
-        """Deploy pyth contract, then get current price for BTC/USD"""
-        self.web3_client.deploy_and_get_contract("./libraries/external/QueryAccount.sol", "0.7.0",
-                                                 account=self.acc)
+        """Deploy pyth contract, then get current price for BTC/USD"""                                     
         contract, _ = self.web3_client.deploy_and_get_contract("./pyth/PythOracle", "0.8.0",
                                                                account=self.acc)
 
@@ -25,10 +23,10 @@ class TestPyth(BaseTests):
 
         latest_price, conf, expo = latest_price_feeds(BTC_USD_ID)
         assert math.isclose(
-            abs(latest_price - int(price[0])), 0.0, rel_tol=conf**expo)
+            abs(latest_price - int(price[0])), 0.0, rel_tol=conf*(10**expo))
 
     @pytest.mark.only_devnet
-    def test_deploy_contract_get_price_pyth_network(self):
+    def test_deploy_contract_pyth_network_get_price(self):
         """Call current price for BTC/USD from another contract"""
         _, contract_deploy_tx = self.web3_client.deploy_and_get_contract("./pyth/PythOracle", "0.8.0",
                                                                          account=self.acc)
@@ -39,7 +37,7 @@ class TestPyth(BaseTests):
         price = contract.functions.get(address, BTC_USD_ID).call()
 
         latest_price, conf, expo = latest_price_feeds(BTC_USD_ID)
-        assert math.isclose(abs(latest_price - price), 0.0, rel_tol=conf**expo)
+        assert math.isclose(abs(latest_price - price), 0.0, rel_tol=conf*(10**expo))
 
 
 def latest_price_feeds(id):

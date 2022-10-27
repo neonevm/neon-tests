a2102bcb62a9b16f2f7d5d67503b55a246f3b4e9
gigimon <gigimon4ik@gmail.com>
2022-10-27 18:14:54 +0300

    * add threads for operator

--- loadtesting/synthetic/users_generator.py
+++ loadtesting/synthetic/users_generator.py
@@ -25,7 +25,7 @@ sol_client = SolanaClient(SOLANA_URL)
 
 
 def create_operator(key):
-    airdrop_resp = sol_client.request_airdrop(key.public_key, 10000)
+    airdrop_resp = sol_client.request_airdrop(key.public_key, 100000000000)
     eth_address = eth_keys.PrivateKey(key.secret_key[:32]).public_key.to_address()
     resp = requests.post(FAUCET_URL,
                          json={"amount": 1000, "wallet": eth_address})

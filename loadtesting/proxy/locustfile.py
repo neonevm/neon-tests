ab80cd24ffe74f714c4c2db2d9e3ca6952d4878d
gigimon <gigimon4ik@gmail.com>
2022-11-15 14:49:45 +0300

    * ask balance in another task in load test

--- loadtesting/proxy/locustfile.py
+++ loadtesting/proxy/locustfile.py
@@ -634,13 +634,13 @@ class NeonTasksSet(NeonProxyTasksSet):
     """Implements Neons transfer base pipeline tasks"""
 
     @task(1)
-    @execute_before("task_block_number", "task_keeps_balance")
+    # @execute_before("task_block_number", "task_keeps_balance")
     def task_send_neon(self) -> tp.Union[None, web3.datastructures.AttributeDict]:
         """Transferring funds to a random account"""
         # add credits to account
         recipient = random.choice(self.user.environment.shared.accounts)
         self.log.info(f"Send `neon` from {str(self.account.address)[-8:]} to {str(recipient.address)[-8:]}.")
-        return self.web3_client.send_neon(self.account, recipient, amount=1)
+        return self.web3_client.send_neon(self.account, recipient, amount=0.00000001)
 
 
 @tag("erc20")

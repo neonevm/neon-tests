a8bea13065197e4d39670a823c478c99970cda69
gigimon <gigimon4ik@gmail.com>
2023-01-03 15:06:31 +0300

    small fixes

--- loadtesting/proxy/locustfile.py
+++ loadtesting/proxy/locustfile.py
@@ -1,3 +1,3 @@
-from loadtesting.proxy.tests.send_neon import NeonUser
-from loadtesting.proxy.tests.erc20 import ERC20User
+from loadtesting.proxy.tests.send_neon import NeonTasksSet
+from loadtesting.proxy.tests.erc20 import ERC20TasksSet
 from loadtesting.proxy.tests.erc20spl import ERC20SPLTasksSet

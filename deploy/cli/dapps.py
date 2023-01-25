e32d52c8c0ac5df66a933af7549d6dabb95a9f54
kristinaNikolaeva <kn@neonlabs.org>
2023-01-25 11:20:10 +0400

    Update dapps.py

--- deploy/cli/dapps.py
+++ deploy/cli/dapps.py
@@ -126,9 +126,6 @@ def upload_service_logs(ssh_client, service, artifact_logs):
     scp_client = SCPClient(transport=ssh_client.get_transport())
     print(f"Upload logs for service: {service}")
     ssh_client.exec_command(f"touch /tmp/{service}.log.bz2")
-    stdin, stdout, stderr = ssh_client.exec_command(f"sudo docker logs {service} -n 10")
-    print(stdout.read())
-    print(stderr.read())
     stdin, stdout, stderr = ssh_client.exec_command(
         f'sudo docker logs {service} 2>&1 | pbzip2 -f > /tmp/{service}.log.bz2')
     print(stdout.read())

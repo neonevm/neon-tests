9052c7963d97cbe083aa999e285ea5e51c86cefa
romanova-natasha <47331640+romanova-natasha@users.noreply.github.com>
2023-02-02 14:02:22 +0300

    NDEV-732 add chainlink tests (#137)

---
+++ contracts/chainlink/ChainlinkOracle.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AggregatorV3Interface.sol";
+import "./libraries/Utils.sol";
+
+contract ChainlinkOracle is AggregatorV3Interface {
+    uint8 public decimals;
+    uint256 public feedAddress;
+    string public description;
+    uint256 public version;
+    uint32 private historicalLength;
+
+    constructor(bytes32 _feedAddress) {
+        feedAddress = uint256(_feedAddress);
+
+        Utils.Header memory header = Utils.getHeader(feedAddress);
+        version = header.version;
+        description = header.description;
+        decimals = header.decimals;
+        // Save historical ringbuffer length for future use.
+        historicalLength = Utils.getHistoricalLength(
+            feedAddress,
+            header.liveLength
+        );
+    }
+
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        Utils.Round memory round = Utils.getRoundbyId(
+            feedAddress,
+            uint32(_roundId),
+            historicalLength
+        );
+
+        return (
+            round.roundId,
+            round.answer,
+            round.timestamp,
+            round.timestamp,
+            round.roundId
+        );
+    }
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        Utils.Round memory round = Utils.getLatestRound(feedAddress);
+
+        return (
+            round.roundId,
+            round.answer,
+            round.timestamp,
+            round.timestamp,
+            round.roundId
+        );
+    }
+}

8740b118d209e66914975b756174291484973f10
kristinaNikolaeva <kn@neonlabs.org>
2023-03-29 11:45:14 +0400

    added tests for SelfDestruct

---
+++ contracts/SelfDestroyable.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.8.4;
+
+contract SelfDestroyable {
+    address payable public owner;
+    bool public isDestroyed;
+    string public text = "text";
+
+    event FunctionCalled(string message);
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function deposit() public payable returns (bool b) {
+        return true;
+    }
+    function destroy(address fundsRecipient) public payable{
+        address payable addr = payable(fundsRecipient);
+        selfdestruct(addr);
+        isDestroyed = true;
+    }
+
+    function anyFunction() public {
+        text = "new text";
+        emit FunctionCalled("hello");
+    }
+
+
+    function transfer1Ether(address recipient) public returns (bool b){
+        require(address(this).balance >= 1 ether, "contract balance less 1 ether");
+        bool success = payable(recipient).send(1 ether);
+        require(success, "transfer failed");
+        return true;
+    }
+
+    receive() external payable {}
+}
+
+contract SelfDestroyableContractCaller {
+    address private contractAddress;
+
+    constructor(address _address) {
+        contractAddress = _address;
+    }
+
+    function callDestroy(address fundsRecipient) public {
+        (bool success, )  = address(contractAddress).call(abi.encodeWithSignature("destroy(address)", fundsRecipient));
+        require(success, "callDestroy: failed call");
+    }
+
+    function callDestroyAndSendMoneyFromContract(address fundsRecipient) public {
+        callDestroy(fundsRecipient);
+        address(contractAddress).call(abi.encodeWithSignature("transfer1Ether(address)", fundsRecipient));
+    }
+
+    function sendMoneyFromContractAndCallDestroy(address fundsRecipient) public payable {
+        address(contractAddress).call(abi.encodeWithSignature("transfer1Ether(address)", fundsRecipient));
+        callDestroy(fundsRecipient);
+    }
+    function callDestroyTwice(address fundsRecipient) public {
+        callDestroy(fundsRecipient);
+        callDestroy(fundsRecipient);
+    }
+}

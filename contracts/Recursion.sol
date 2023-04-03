187db7fcacb0490bb2be399c57f5ad396eb0c869
kristinaNikolaeva <kn@neonlabs.org>
2023-04-03 18:50:07 +0400

    added 3 new tests

--- contracts/Recursion.sol
+++ contracts/Recursion.sol
@@ -1,22 +1,22 @@
 pragma solidity ^0.8.0;
 
-contract FirstContract {
+contract DeployRecursionFactory {
+    address [] public firstContractAddresses;
     address [] public secondContractAddresses;
-    address [] public thirdContractAddresses;
     uint private depth;
     uint private depthCounter;
+    event FirstContractDeployed(address addr);
     event SecondContractDeployed(address addr);
-    event ThirdContractDeployed(address addr);
     constructor(uint _depth) {
         setDepth(_depth);
     }
 
-    function deploySecondContract() public {
+    function deployFirstContract() public {
         if (depthCounter < depth) {
             depthCounter+=1;
-            address secondContract = address(new SecondContract(address(this)));
-            emit SecondContractDeployed(secondContract);
-            secondContractAddresses.push(secondContract);
+            address firstContract = address(new FirstContract(address(this)));
+            emit FirstContractDeployed(firstContract);
+            firstContractAddresses.push(firstContract);
         }
     }
 
@@ -25,52 +25,110 @@ contract FirstContract {
         depthCounter = 0;
     }
 
+    function getFirstDeployedContractCount() public view returns (uint count) {
+        return firstContractAddresses.length;
+    }
     function getSecondDeployedContractCount() public view returns (uint count) {
         return secondContractAddresses.length;
     }
-    function getThirdDeployedContractCount() public view returns (uint count) {
-        return thirdContractAddresses.length;
-    }
-    function deployThirdContractViaCreate2(string memory stringSalt) public returns (address thirdContract){
+    function deploySecondContractViaCreate2(string memory stringSalt) public returns (address secondContract){
         if (depthCounter < depth) {
         depthCounter+=1;
-        thirdContract = _deployThirdContractViaCreate2(stringSalt);
+        secondContract = _deploySecondContractViaCreate2(stringSalt);
         }
-        return thirdContract;
+        return secondContract;
         }
 
-    function _deployThirdContractViaCreate2(string memory stringSalt) public returns (address thirdContract){
-        bytes memory bytecode = type(ThirdContract).creationCode;
+        function deployFirstContractViaCreate() public returns (address firstContract){
+        if (depthCounter < depth) {
+        depthCounter+=1;
+        firstContract = _deployFirstContractViaCreate();
+        }
+        return firstContract;
+        }
+
+    function _deploySecondContractViaCreate2(string memory stringSalt) public returns (address secondContract){
+        bytes memory bytecode = type(SecondContract).creationCode;
         bytecode = abi.encodePacked(bytecode, abi.encode(address(this), stringSalt));
-        
+
         bytes32 salt = keccak256(abi.encodePacked(stringSalt));
         assembly {
-            thirdContract := create2(0, add(bytecode, 32), mload(bytecode), salt)
+            secondContract := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        thirdContractAddresses.push(thirdContract);
-        emit ThirdContractDeployed(thirdContract);
+        secondContractAddresses.push(secondContract);
+        emit SecondContractDeployed(secondContract);
     }
+
+    function _deployFirstContractViaCreate() public returns (address firstContract){
+        bytes memory bytecode = type(FirstContract).creationCode;
+        bytecode = abi.encodePacked(bytecode, abi.encode(address(this)));
+        assembly {
+            firstContract := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+        secondContractAddresses.push(firstContract);
+        emit SecondContractDeployed(firstContract);
+    }
+
+
 
     function deployViaCreate2Twice(string memory stringSalt) public {
-        _deployThirdContractViaCreate2(stringSalt);
-        _deployThirdContractViaCreate2(stringSalt);
+        _deploySecondContractViaCreate2(stringSalt);
+        _deploySecondContractViaCreate2(stringSalt);
     }
 
+}
+
+contract FirstContract {
+    constructor(address _factoryAddress) {
+        address(_factoryAddress).call(abi.encodeWithSignature("deploySecondContract()"));
+    }
 }
 
 contract SecondContract {
-    address public firstContract;
-
-    constructor(address _firstContractAddress) {
-        address(_firstContractAddress).call(abi.encodeWithSignature("deploySecondContract()"));
+    constructor(address _factoryAddress, string memory stringSalt) {
+        address(_factoryAddress).call{gas: 100000}(abi.encodeWithSignature("deployThirdContractViaCreate2(string)", stringSalt));
     }
+
 }
 
-contract ThirdContract {
-    address public firstContract;
 
-    constructor(address _firstContractAddress, string memory stringSalt) {
-        address(_firstContractAddress).call{gas: 100000}(abi.encodeWithSignature("deployThirdContractViaCreate2(string)", stringSalt));
+contract RecursionCaller1 {
+    uint public depth;
+    uint private depthCounter;
+    address private contract2;
+    event SecondContractCalled(bool result);
+
+
+    constructor(uint _depth, address _contract2, bool setDepthThroughContract2) {
+        contract2 = _contract2;
+        if (setDepthThroughContract2){
+          address(contract2).call(abi.encodeWithSignature("callContract1SetDepth(address,uint)", address(this), _depth));
+        }
+        else {
+            setDepth(_depth);
+        }
     }
 
+    function setDepth(uint _depth) public {
+        depth = _depth;
+        depthCounter = 0;
+    }
+    function callContract2() public {
+        if (depthCounter < depth) {
+            depthCounter+=1;
+            (bool success, ) = address(contract2).call(abi.encodeWithSignature("callContract1Recursion(address)", address(this)));
+            emit SecondContractCalled(success);
+        }
+    }
+
+}
+
+contract RecursionCaller2 {
+    function callContract1Recursion(address _address) public {
+        address(_address).call(abi.encodeWithSignature("callContract2()"));
+    }
+    function callContract1SetDepth(address _address, uint depth) public {
+        address(_address).call(abi.encodeWithSignature("setDepth(uint)",depth));
+
+    }
 }
--- integration/tests/basic/opcodes/test_recursion_contract_deploy.py
+++ integration/tests/basic/opcodes/test_recursion_contract_deploy.py
@@ -12,47 +12,86 @@ ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 class TestContractRecursion(BaseMixin):
 
     @pytest.fixture(scope="function")
-    def first_contract(self):
-        contract, trx = self.web3_client.deploy_and_get_contract(
+    def recursion_factory(self):
+        contract, _ = self.web3_client.deploy_and_get_contract(
             "Recursion", "0.8.10", self.sender_account,
-            contract_name="FirstContract",
+            contract_name="DeployRecursionFactory",
             constructor_args=[3])
         return contract
 
-    def test_deploy_with_recursion(self, first_contract):
+    def test_deploy_with_recursion(self, recursion_factory):
         tx = self.create_contract_call_tx_object(self.sender_account)
-        instruction_tx = first_contract.functions.deploySecondContract().buildTransaction(tx)
+        instruction_tx = recursion_factory.functions.deployFirstContract().build_transaction(tx)
         receipt = self.web3_client.send_transaction(self.sender_account, instruction_tx)
         assert receipt["status"] == 1
-        assert first_contract.functions.getSecondDeployedContractCount().call() == 3
+        assert recursion_factory.functions.getFirstDeployedContractCount().call() == 3
 
-        event_logs = first_contract.events.SecondContractDeployed().processReceipt(receipt)
+        event_logs = recursion_factory.events.FirstContractDeployed().process_receipt(receipt)
         for event_log in event_logs:
             assert event_log["args"]["addr"] != ZERO_ADDRESS
 
     @pytest.mark.xfail(reason="SA-159")
-    def test_deploy_with_recursion_via_create2(self, first_contract):
+    def test_deploy_with_recursion_via_create2(self, recursion_factory):
         tx = self.create_contract_call_tx_object(self.sender_account)
         salt = generate_text(min_len=5, max_len=7)
-        instruction_tx = first_contract.functions.deployThirdContractViaCreate2(salt).buildTransaction(tx)
+        instruction_tx = recursion_factory.functions.deploySecondContractViaCreate2(salt).build_transaction(tx)
         receipt = self.web3_client.send_transaction(self.sender_account, instruction_tx)
         assert receipt["status"] == 1
 
-        event_logs = first_contract.events.ThirdContractDeployed().processReceipt(receipt)
+        event_logs = recursion_factory.events.SecondContractDeployed().process_receipt(receipt)
         addresses = [event_log["args"]["addr"] for event_log in event_logs]
         assert len(addresses) == 1
         assert ZERO_ADDRESS not in addresses
 
-    @pytest.mark.xfail(reason="SA-159")
-    def test_deploy_to_the_same_address_via_create2_one_trx(self, first_contract):
+    def test_deploy_with_recursion_via_create(self, recursion_factory):
         tx = self.create_contract_call_tx_object(self.sender_account)
-        salt = generate_text(min_len=5, max_len=7)
-        instruction_tx = first_contract.functions.deployViaCreate2Twice(salt).buildTransaction(tx)
+        instruction_tx = recursion_factory.functions.deployFirstContractViaCreate().build_transaction(tx)
         receipt = self.web3_client.send_transaction(self.sender_account, instruction_tx)
         assert receipt["status"] == 1
-        assert first_contract.functions.getThirdDeployedContractCount().call() == 2
 
-        event_logs = first_contract.events.ThirdContractDeployed().processReceipt(receipt)
+        event_logs = recursion_factory.events.FirstContractDeployed().process_receipt(receipt)
+        addresses = [event_log["args"]["addr"] for event_log in event_logs]
+        assert len(addresses) == 3
+        assert ZERO_ADDRESS not in addresses
+
+    @pytest.mark.xfail(reason="SA-159")
+    def test_deploy_to_the_same_address_via_create2_one_trx(self, recursion_factory):
+        tx = self.create_contract_call_tx_object(self.sender_account)
+        salt = generate_text(min_len=5, max_len=7)
+        instruction_tx = recursion_factory.functions.deployViaCreate2Twice(salt).build_transaction(tx)
+        receipt = self.web3_client.send_transaction(self.sender_account, instruction_tx)
+        assert receipt["status"] == 1
+        assert recursion_factory.functions.getSecondDeployedContractCount().call() == 2
+
+        event_logs = recursion_factory.events.SecondContractDeployed().process_receipt(receipt)
         addresses = [event_log["args"]["addr"] for event_log in event_logs]
         assert ZERO_ADDRESS in addresses
 
+    def test_recursion_in_function_calls(self):
+        contract_caller2, _ = self.web3_client.deploy_and_get_contract(
+            "Recursion", "0.8.10", self.sender_account,
+            contract_name="RecursionCaller2")
+        depth = 5
+        contract_caller1, _ = self.web3_client.deploy_and_get_contract(
+            "Recursion", "0.8.10", self.sender_account,
+            contract_name="RecursionCaller1",
+            constructor_args=[depth, contract_caller2.address, False])
+        tx = self.create_contract_call_tx_object(self.sender_account)
+        instruction_tx = contract_caller1.functions.callContract2().build_transaction(tx)
+        receipt = self.web3_client.send_transaction(self.sender_account, instruction_tx)
+        assert receipt["status"] == 1
+        event_logs = contract_caller1.events.SecondContractCalled().process_receipt(receipt)
+        assert len(event_logs) == depth
+        results = [event_log["args"]["result"] for event_log in event_logs]
+        assert False not in results
+
+    def test_recursion_in_constructor_calls(self):
+        contract_caller2, _ = self.web3_client.deploy_and_get_contract(
+            "Recursion", "0.8.10", self.sender_account,
+            contract_name="RecursionCaller2")
+        contract_caller1, _ = self.web3_client.deploy_and_get_contract(
+            "Recursion", "0.8.10", self.sender_account,
+            contract_name="RecursionCaller1",
+            constructor_args=[5, contract_caller2.address, True])
+
+        assert contract_caller1.functions.depth().call() == 0

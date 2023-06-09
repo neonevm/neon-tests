228af83a218568df6fabd1c4a4cce3b54272ba13
gigimon <gigimon4ik@gmail.com>
2023-06-09 15:29:17 +0300

    add wait for hardhat compile

---
+++ deploy/infra/compile_contracts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+path="~/.cache/hardhat-nodejs/compilers-v2/linux-amd64/"
+attempts=1
+
+while [ $attempts -le 10 ]; do
+  echo "Attempt $attempts"
+
+  npx hardhat compile
+  wait $!
+
+  file_count=$(ls -1 $path | wc -l)
+
+  if [ $file_count -gt 2 ]; then
+    echo "Hardhat compiled files successfully"
+    exit 0
+  else
+    echo "Looks like hardhat didn't download compiler, retry"
+    sleep 15
+    attempts=$((attempts+1))
+  fi
+done
+
+echo "Failed after 10 attempts"
+exit 1

60bac14b2a9f66e64e598d4b01ccb4312d00600d
kristinaNikolaeva <kn@neonlabs.org>
2023-06-14 16:01:56 +0300

    Update clickfile.py

--- clickfile.py
+++ clickfile.py
@@ -424,13 +424,13 @@ def contracts():
 
     response = requests.get(
         "https://api.github.com/repos/neonlabsorg/neon-evm/contents/evm_loader/solidity?ref=develop"
-    ).json()
+    )
     if response.status_code != 200:
         raise click.ClickException(
-            f"The code is not 200. Responce: {response}"
+            f"The code is not 200. Responce: {response.json()}"
         )
 
-    for item in response:
+    for item in response.json():
         r = requests.get(
             f"https://raw.githubusercontent.com/neonlabsorg/neon-evm/develop/evm_loader/solidity/{item['name']}"
         )

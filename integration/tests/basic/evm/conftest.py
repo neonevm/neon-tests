379af5bb31a65abfccad09f9bcc0bc8b4210f7cc
kristinaNikolaeva <kn@neonlabs.org>
2023-07-12 18:36:03 +0300

    NDEV-1652 added tests for precompiled metaplex contract

---
+++ integration/tests/basic/evm/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+from utils import helpers
+
+METAPLEX_ADDRESS = "0xff00000000000000000000000000000000000005"
+
+
+@pytest.fixture(scope="class")
+def metaplex_caller(web3_client, class_account):
+    contract, _ = web3_client.deploy_and_get_contract(
+        "metaplex_caller.sol", "0.8.10", account=class_account, contract_name="metaplexCaller"
+    )
+    return contract
+
+
+@pytest.fixture(scope="class")
+def metaplex(web3_client):
+    contract_interface = helpers.get_contract_interface("Metaplex", "0.8.10",
+                                                        contract_name="Metaplex")
+    contract = web3_client.eth.contract(address=METAPLEX_ADDRESS,
+                                        abi=contract_interface["abi"])
+    return contract

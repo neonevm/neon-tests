32d7ed461ac92b7f67ad2bdf09660fa624f69730
gigimon <gigimon4ik@gmail.com>
2023-08-28 10:24:04 +0300

    * fix economy tests after vyper

--- utils/web3client.py
+++ utils/web3client.py
@@ -4,7 +4,6 @@ import time
 import typing as tp
 from decimal import Decimal
 
-import vyper
 import web3
 import web3.types
 import requests
@@ -17,7 +16,9 @@ from utils.helpers import decode_function_signature
 
 
 class NeonWeb3Client:
-    def __init__(self, proxy_url: str, chain_id: int, session: tp.Optional[tp.Any] = None):
+    def __init__(
+        self, proxy_url: str, chain_id: int, session: tp.Optional[tp.Any] = None
+    ):
         self._proxy_url = proxy_url
         self._web3 = web3.Web3(web3.HTTPProvider(proxy_url, session=session))
         self._chain_id = chain_id
@@ -27,7 +28,10 @@ class NeonWeb3Client:
         return getattr(self._web3, item)
 
     def _get_evm_info(self, method):
-        resp = requests.post(self._proxy_url, json={"jsonrpc": "2.0", "method": method, "params": [], "id": 1})
+        resp = requests.post(
+            self._proxy_url,
+            json={"jsonrpc": "2.0", "method": method, "params": [], "id": 1},
+        )
         resp.raise_for_status()
         try:
             body = resp.json()
@@ -43,17 +47,27 @@ class NeonWeb3Client:
 
     def get_evm_version(self):
         return self._get_evm_info("web3_clientVersion")
-    
+
     def get_neon_emulate(self, params):
         return requests.post(
             self._proxy_url,
-            json={"jsonrpc": "2.0", "method": "neon_emulate", "params": [params], "id": 0},
+            json={
+                "jsonrpc": "2.0",
+                "method": "neon_emulate",
+                "params": [params],
+                "id": 0,
+            },
         ).json()
 
     def get_solana_trx_by_neon(self, tr_id: str):
         return requests.post(
             self._proxy_url,
-            json={"jsonrpc": "2.0", "method": "neon_getSolanaTransactionByNeonTransaction", "params": [tr_id], "id": 0},
+            json={
+                "jsonrpc": "2.0",
+                "method": "neon_getSolanaTransactionByNeonTransaction",
+                "params": [tr_id],
+                "id": 0,
+            },
         ).json()
 
     def gas_price(self):
@@ -64,28 +78,41 @@ class NeonWeb3Client:
         return self._web3.eth.account.create()
 
     def create_account_with_balance(
-            self, faucet, amount: int = InputTestConstants.FAUCET_1ST_REQUEST_AMOUNT.value, bank_account=None
+        self,
+        faucet,
+        amount: int = InputTestConstants.FAUCET_1ST_REQUEST_AMOUNT.value,
+        bank_account=None,
     ):
         """Creates a new account with balance"""
         account = self.create_account()
-        balance_before = float(self.from_wei(self.eth.get_balance(account.address), Unit.ETHER))
+        balance_before = float(
+            self.from_wei(self.eth.get_balance(account.address), Unit.ETHER)
+        )
 
         if bank_account is not None:
             self.send_neon(bank_account, account, amount)
         else:
             faucet.request_neon(account.address, amount=amount)
         for _ in range(20):
-            if float(self.from_wei(self.eth.get_balance(account.address), Unit.ETHER)) >= (balance_before + amount):
+            if float(
+                self.from_wei(self.eth.get_balance(account.address), Unit.ETHER)
+            ) >= (balance_before + amount):
                 break
             time.sleep(1)
         else:
-            raise AssertionError(f"Balance didn't changed after 20 seconds ({account.address})")
+            raise AssertionError(
+                f"Balance didn't changed after 20 seconds ({account.address})"
+            )
         return account
 
-    def get_balance(self, address: tp.Union[str, eth_account.signers.local.LocalAccount]):
+    def get_balance(
+        self, address: tp.Union[str, eth_account.signers.local.LocalAccount]
+    ):
         if not isinstance(address, str):
             address = address.address
-        return web3.Web3.from_wei(self._web3.eth.get_balance(address, "pending"), "ether")
+        return web3.Web3.from_wei(
+            self._web3.eth.get_balance(address, "pending"), "ether"
+        )
 
     def get_block_number(self):
         return self._web3.eth.get_block_number()
@@ -93,7 +120,11 @@ class NeonWeb3Client:
     def get_block_number_by_id(self, block_identifier):
         return self._web3.eth.get_block(block_identifier)
 
-    def get_nonce(self, address: tp.Union[eth_account.signers.local.LocalAccount, str], block: str = "pending"):
+    def get_nonce(
+        self,
+        address: tp.Union[eth_account.signers.local.LocalAccount, str],
+        block: str = "pending",
+    ):
         address = address if isinstance(address, str) else address.address
         return self._web3.eth.get_transaction_count(address, block)
 
@@ -104,7 +135,7 @@ class NeonWeb3Client:
         amount: tp.Union[int, float, Decimal],
         gas: tp.Optional[int] = 0,
         gas_price: tp.Optional[int] = None,
-        nonce: int = None
+        nonce: int = None,
     ) -> web3.types.TxReceipt:
         to_addr = to if isinstance(to, str) else to.address
         if nonce is None:
@@ -187,8 +218,12 @@ class NeonWeb3Client:
         return self._web3.eth.wait_for_transaction_receipt(tx)
 
     def send_transaction(
-        self, account: eth_account.signers.local.LocalAccount, transaction: tp.Dict,
-            gas_multiplier: tp.Optional[float] = None  # fix for some event depends transactions
+        self,
+        account: eth_account.signers.local.LocalAccount,
+        transaction: tp.Dict,
+        gas_multiplier: tp.Optional[
+            float
+        ] = None,  # fix for some event depends transactions
     ) -> web3.types.TxReceipt:
         if "gasPrice" not in transaction:
             transaction["gasPrice"] = self.gas_price()
@@ -196,7 +231,9 @@ class NeonWeb3Client:
             transaction["gas"] = self._web3.eth.estimate_gas(transaction)
         if gas_multiplier is not None:
             transaction["gas"] = int(transaction["gas"] * gas_multiplier)
-        instruction_tx = self._web3.eth.account.sign_transaction(transaction, account.key)
+        instruction_tx = self._web3.eth.account.sign_transaction(
+            transaction, account.key
+        )
         signature = self._web3.eth.send_raw_transaction(instruction_tx.rawTransaction)
         return self._web3.eth.wait_for_transaction_receipt(signature)
 
@@ -210,9 +247,12 @@ class NeonWeb3Client:
         import_remapping: tp.Optional[dict] = None,
         gas: tp.Optional[int] = 0,
     ) -> tp.Tuple[tp.Any, web3.types.TxReceipt]:
-        contract_interface = helpers.get_contract_interface(contract, version,
-                                                            contract_name=contract_name,
-                                                            import_remapping=import_remapping)
+        contract_interface = helpers.get_contract_interface(
+            contract,
+            version,
+            contract_name=contract_name,
+            import_remapping=import_remapping,
+        )
 
         contract_deploy_tx = self.deploy_contract(
             account,
@@ -222,28 +262,37 @@ class NeonWeb3Client:
             gas=gas,
         )
 
-        contract = self.eth.contract(address=contract_deploy_tx["contractAddress"], abi=contract_interface["abi"])
+        contract = self.eth.contract(
+            address=contract_deploy_tx["contractAddress"], abi=contract_interface["abi"]
+        )
 
         return contract, contract_deploy_tx
 
-    def compile_by_vyper_and_deploy(self, account, contract_name, constructor_args=None):
+    def compile_by_vyper_and_deploy(
+        self, account, contract_name, constructor_args=None
+    ):
+        import vyper  # Import here because vyper prevent override decimal precision (uses in economy tests)
+
         contract_path = pathlib.Path.cwd() / "contracts" / "vyper"
-        with open(contract_path / f'{contract_name}.vy') as f:
+        with open(contract_path / f"{contract_name}.vy") as f:
             contract_code = f.read()
-            contract_interface = vyper.compile_code(contract_code, output_formats=["abi", "bytecode"])
+            contract_interface = vyper.compile_code(
+                contract_code, output_formats=["abi", "bytecode"]
+            )
 
         contract_deploy_tx = self.deploy_contract(
             account,
             abi=contract_interface["abi"],
             bytecode=contract_interface["bytecode"],
-            constructor_args=constructor_args
+            constructor_args=constructor_args,
         )
-        return self.eth.contract(address=contract_deploy_tx["contractAddress"],
-                                        abi=contract_interface["abi"])
+        return self.eth.contract(
+            address=contract_deploy_tx["contractAddress"], abi=contract_interface["abi"]
+        )
 
     @staticmethod
     def text_to_bytes32(text: str) -> bytes:
-        return text.encode().ljust(32, b'\0')
+        return text.encode().ljust(32, b"\0")
 
     def call_function_at_address(self, contract_address, signature, args, result_types):
         calldata = decode_function_signature(signature, args)
@@ -253,4 +302,3 @@ class NeonWeb3Client:
         }
         result = self._web3.eth.call(tx)
         return abi.decode(result_types, result)[0]
-

deb3db4d21c510024c6a9a8ec7e20ff35f958d33
kristinaNikolaeva <kn@neonlabs.org>
2023-09-20 14:53:46 +0200

    added logs

--- deploy/cli/github_api_client.py
+++ deploy/cli/github_api_client.py
@@ -10,9 +10,9 @@ class GithubClient:
 
     def add_comment_to_pr(self, url, msg):
         data = {"body": f"```{msg}```"}
-        response = requests.post(url, json=data, headers=self.headers)
         click.echo(f"Sent data: {data}")
         click.echo(f"Headers: {self.headers}")
+        response = requests.post(url, json=data, headers=self.headers)
         click.echo(f"Status code: {response.status_code}")
         if response.status_code != 204:
             raise RuntimeError(f"Attempt to leave a comment on a PR failed: {response.text}")

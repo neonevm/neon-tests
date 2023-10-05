5c81722c3306fc52ddc7bdbb6c8550262e585851
Aleksandr Liubenkov <lubenkov.a@gmail.com>
2023-10-05 15:30:16 +0300

    Move all the rest test_eth_get_transaction_* to dedicated class

--- integration/tests/basic/rpc/test_rpc_base_calls.py
+++ integration/tests/basic/rpc/test_rpc_base_calls.py
@@ -12,8 +12,7 @@ from integration.tests.basic.helpers import rpc_checks
 from integration.tests.basic.helpers.assert_message import AssertMessage
 from integration.tests.basic.helpers.basic import BaseMixin
 from integration.tests.basic.helpers.errors import Error32000, Error32602
-from integration.tests.basic.helpers.rpc_checks import is_hex, assert_fields_are_hex, assert_equal_fields, \
-    hex_str_consists_not_only_of_zeros
+from integration.tests.basic.helpers.rpc_checks import is_hex, hex_str_consists_not_only_of_zeros
 from integration.tests.helpers.basic import cryptohex
 from utils import helpers
 from utils.helpers import gen_hash_of_block
@@ -127,15 +126,6 @@ class TestRpcBaseCalls(BaseMixin):
                 response["result"] == "0x"
         ), f"Invalid response result, `{response['result']}`"
 
-    def test_eth_get_transaction_receipt_with_incorrect_hash(self):
-        """Verify implemented rpc calls work eth_getTransactionReceipt when transaction hash is not correct"""
-
-        response = self.proxy_api.send_rpc(
-            method="eth_getTransactionReceipt", params=gen_hash_of_block(31)
-        )
-        assert "error" in response
-        assert response["error"]["message"] == "transaction-id is not hex"
-
     def test_eth_gas_price(self):
         """Verify implemented rpc calls work eth_gasPrice"""
         response = self.proxy_api.send_rpc("eth_gasPrice")
@@ -210,21 +200,6 @@ class TestRpcBaseCalls(BaseMixin):
         assert int(response["result"]) == self.web3_client._chain_id, \
             f"Invalid response result {response['result']}"
 
-    @pytest.mark.parametrize("param", [Tag.LATEST, Tag.PENDING, Tag.EARLIEST, None])
-    def test_eth_get_transaction_count(self, param: tp.Union[Tag, None]):
-        """Verify implemented rpc calls work eth_getTransactionCount"""
-        if param:
-            self.send_neon(self.sender_account, self.recipient_account, 1)
-            param = [self.sender_account.address, param.value]
-        response = self.proxy_api.send_rpc("eth_getTransactionCount", params=param)
-        if not param:
-            assert "error" in response, "Error not in response"
-            return
-        assert "error" not in response
-        assert rpc_checks.is_hex(
-            response["result"]
-        ), AssertMessage.DOES_NOT_START_WITH_0X.value
-
     def test_eth_send_raw_transaction(self):
         """Verify implemented rpc calls work eth_sendRawTransaction"""
         transaction = self.create_tx_object(amount=1)
@@ -264,98 +239,6 @@ class TestRpcBaseCalls(BaseMixin):
             contract_name="ValueOf24K", account=self.sender_account
         )
         assert rpc_checks.is_hex(contract.address)
-
-    @pytest.mark.parametrize("param", [32, 16, None])
-    def test_eth_get_transaction_by_hash_negative(self, param: tp.Union[int, None]):
-        response = self.proxy_api.send_rpc(
-            method="eth_getTransactionByHash",
-            params=gen_hash_of_block(param) if param else param,
-        )
-
-        if param is pow(2, 5):
-            assert "error" not in response
-            assert response["result"] is None, f"Invalid response: {response['result']}"
-            return
-
-        assert "error" in response, "error field not in response"
-        assert "code" in response["error"]
-        assert "message" in response["error"], "message field not in response"
-        code = response["error"]["code"]
-        message = response["error"]["message"]
-        if param is None:
-            assert code == Error32000.CODE, "wrong code"
-            assert Error32000.MISSING_ARGUMENT in message, "wrong message"
-            return
-
-        assert code == Error32602.CODE, "wrong code"
-        assert Error32602.NOT_HEX in message, "wrong message"
-
-    def test_eth_get_transaction_by_hash(self):
-        receipt = self.send_neon(
-            self.sender_account, self.recipient_account, amount=0.001
-        )
-        response = self.proxy_api.send_rpc(
-            method="eth_getTransactionByHash",
-            params=receipt["transactionHash"].hex(),
-        )
-        assert "error" not in response
-        result = response["result"]
-        assert_fields_are_hex(result, [
-            "blockHash",
-            "blockNumber",
-            "hash",
-            "transactionIndex",
-            "type",
-            "from",
-            "nonce",
-            "gasPrice",
-            "gas",
-            "to"
-        ])
-        assert_equal_fields(result, receipt, [
-            "blockHash",
-            "blockNumber",
-            "hash",
-            "transactionIndex",
-            "type",
-            "from",
-            "to"
-        ], {"hash": "transactionHash"})
-
-    def test_eth_get_transaction_receipt(self):
-        """Verify implemented rpc calls work eth_getTransactionReceipt"""
-        tx_receipt = self.send_neon(self.sender_account, self.recipient_account, 10)
-        transaction_hash = tx_receipt.transactionHash.hex()
-        response = self.proxy_api.send_rpc(
-            method="eth_getTransactionReceipt", params=transaction_hash
-        )
-        assert "error" not in response
-        assert "result" in response, AssertMessage.DOES_NOT_CONTAIN_RESULT
-        result = response["result"]
-        assert_fields_are_hex(result, [
-            "transactionHash",
-            "transactionIndex",
-            "blockNumber",
-            "blockHash",
-            "cumulativeGasUsed",
-            "gasUsed",
-            "logsBloom",
-            "status",
-        ])
-        assert result["status"] == "0x1", "Transaction status must be 0x1"
-        assert result["transactionHash"] == transaction_hash
-        assert result["blockHash"] == tx_receipt.blockHash.hex()
-        assert result["from"].upper() == tx_receipt["from"].upper()
-        assert result["to"].upper() == tx_receipt["to"].upper()
-        assert result["contractAddress"] is None
-        assert result["logs"] == []
-
-    def test_eth_get_transaction_receipt_when_hash_doesnt_exist(self):
-        """Verify implemented rpc calls work eth_getTransactionReceipt when transaction hash doesn't exist"""
-        response = self.proxy_api.send_rpc(
-            method="eth_getTransactionReceipt", params=gen_hash_of_block(32)
-        )
-        assert response["result"] is None, "Result should be None"
 
     def test_eth_block_number(self):
         """Verify implemented rpc calls work work eth_blockNumber"""
--- integration/tests/basic/rpc/test_rpc_get_transaction.py
+++ integration/tests/basic/rpc/test_rpc_get_transaction.py
@@ -5,9 +5,13 @@ import pytest
 import web3
 
 from integration.tests.basic.helpers import rpc_checks
+from integration.tests.basic.helpers.assert_message import AssertMessage
 from integration.tests.basic.helpers.basic import BaseMixin
+from integration.tests.basic.helpers.errors import Error32000, Error32602
+from integration.tests.basic.helpers.rpc_checks import assert_fields_are_hex, assert_equal_fields
 from integration.tests.basic.rpc.test_rpc_base_calls import Tag
 from utils.consts import Unit
+from utils.helpers import gen_hash_of_block
 
 
 @allure.feature("JSON-RPC-GET-TRANSACTION validation")
@@ -114,3 +118,117 @@ class TestRpcGetTransaction(BaseMixin):
                     assert rpc_checks.is_hex(
                         result[field]
                     ), f"Field {field} must be hex but '{result[field]}'"
+
+    def test_eth_get_transaction_receipt_with_incorrect_hash(self):
+        """Verify implemented rpc calls work eth_getTransactionReceipt when transaction hash is not correct"""
+
+        response = self.proxy_api.send_rpc(
+            method="eth_getTransactionReceipt", params=gen_hash_of_block(31)
+        )
+        assert "error" in response
+        assert response["error"]["message"] == "transaction-id is not hex"
+
+    @pytest.mark.parametrize("param", [Tag.LATEST, Tag.PENDING, Tag.EARLIEST, None])
+    def test_eth_get_transaction_count(self, param: tp.Union[Tag, None]):
+        """Verify implemented rpc calls work eth_getTransactionCount"""
+        if param:
+            self.send_neon(self.sender_account, self.recipient_account, 1)
+            param = [self.sender_account.address, param.value]
+        response = self.proxy_api.send_rpc("eth_getTransactionCount", params=param)
+        if not param:
+            assert "error" in response, "Error not in response"
+            return
+        assert "error" not in response
+        assert rpc_checks.is_hex(response["result"]), AssertMessage.DOES_NOT_START_WITH_0X.value
+
+    @pytest.mark.parametrize("param", [32, 16, None])
+    def test_eth_get_transaction_by_hash_negative(self, param: tp.Union[int, None]):
+        response = self.proxy_api.send_rpc(
+            method="eth_getTransactionByHash",
+            params=gen_hash_of_block(param) if param else param,
+        )
+
+        if param is pow(2, 5):
+            assert "error" not in response
+            assert response["result"] is None, f"Invalid response: {response['result']}"
+            return
+
+        assert "error" in response, "error field not in response"
+        assert "code" in response["error"]
+        assert "message" in response["error"], "message field not in response"
+        code = response["error"]["code"]
+        message = response["error"]["message"]
+        if param is None:
+            assert code == Error32000.CODE, "wrong code"
+            assert Error32000.MISSING_ARGUMENT in message, "wrong message"
+            return
+
+        assert code == Error32602.CODE, "wrong code"
+        assert Error32602.NOT_HEX in message, "wrong message"
+
+    def test_eth_get_transaction_by_hash(self):
+        receipt = self.send_neon(
+            self.sender_account, self.recipient_account, amount=0.001
+        )
+        response = self.proxy_api.send_rpc(
+            method="eth_getTransactionByHash",
+            params=receipt["transactionHash"].hex(),
+        )
+        assert "error" not in response
+        result = response["result"]
+        assert_fields_are_hex(result, [
+            "blockHash",
+            "blockNumber",
+            "hash",
+            "transactionIndex",
+            "type",
+            "from",
+            "nonce",
+            "gasPrice",
+            "gas",
+            "to"
+        ])
+        assert_equal_fields(result, receipt, [
+            "blockHash",
+            "blockNumber",
+            "hash",
+            "transactionIndex",
+            "type",
+            "from",
+            "to"
+        ], {"hash": "transactionHash"})
+
+    def test_eth_get_transaction_receipt(self):
+        """Verify implemented rpc calls work eth_getTransactionReceipt"""
+        tx_receipt = self.send_neon(self.sender_account, self.recipient_account, 10)
+        transaction_hash = tx_receipt.transactionHash.hex()
+        response = self.proxy_api.send_rpc(
+            method="eth_getTransactionReceipt", params=transaction_hash
+        )
+        assert "error" not in response
+        assert "result" in response, AssertMessage.DOES_NOT_CONTAIN_RESULT
+        result = response["result"]
+        assert_fields_are_hex(result, [
+            "transactionHash",
+            "transactionIndex",
+            "blockNumber",
+            "blockHash",
+            "cumulativeGasUsed",
+            "gasUsed",
+            "logsBloom",
+            "status",
+        ])
+        assert result["status"] == "0x1", "Transaction status must be 0x1"
+        assert result["transactionHash"] == transaction_hash
+        assert result["blockHash"] == tx_receipt.blockHash.hex()
+        assert result["from"].upper() == tx_receipt["from"].upper()
+        assert result["to"].upper() == tx_receipt["to"].upper()
+        assert result["contractAddress"] is None
+        assert result["logs"] == []
+
+    def test_eth_get_transaction_receipt_when_hash_doesnt_exist(self):
+        """Verify implemented rpc calls work eth_getTransactionReceipt when transaction hash doesn't exist"""
+        response = self.proxy_api.send_rpc(
+            method="eth_getTransactionReceipt", params=gen_hash_of_block(32)
+        )
+        assert response["result"] is None, "Result should be None"

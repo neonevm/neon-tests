fac0284ff26474cd19578b403f24cefd5ff59d2e
gigimon <gigimon4ik@gmail.com>
2024-02-09 11:36:48 +0300

    * disable some tests for devnet (problem with p2p solana)

--- integration/tests/basic/erc/test_ERC20SPL.py
+++ integration/tests/basic/erc/test_ERC20SPL.py
@@ -523,6 +523,7 @@ class TestERC20wrapperContract:
         assert contract_balance_before - contract_balance_after == amount, "Contract balance is not correct"
         assert sol_balance_after == sol_balance_before, "Sol balance is changed"
 
+    @pytest.mark.only_stands
     @pytest.mark.parametrize("mintable", [True, False])
     def test_approveSolana(
         self,

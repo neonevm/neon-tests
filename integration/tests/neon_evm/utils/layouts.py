6a3626a9a4067720f0219609b35567a9e7bb2ad9
kristinaNikolaeva <kn@neonlabs.org>
2024-02-19 13:05:24 +0100

    removed STORAGE_ACCOUNT_INFO_LAYOUT using

--- integration/tests/neon_evm/utils/layouts.py
+++ integration/tests/neon_evm/utils/layouts.py
@@ -1,19 +1,5 @@
 from construct import Bytes, Int8ul, Struct, Int64ul, Int32ul
 
-STORAGE_ACCOUNT_INFO_LAYOUT = Struct(
-    "tag" / Int8ul,
-    "revision" / Int32ul,
-    "owner" / Bytes(32),
-    "hash" / Bytes(32),
-    "caller" / Bytes(20),
-    "chain_id" / Int64ul,
-    "gas_limit" / Bytes(32),
-    "gas_price" / Bytes(32),
-    "gas_used" / Bytes(32),
-    "operator" / Bytes(32),
-    "slot" / Int64ul,
-    "account_list_len" / Int64ul,
-)
 
 HOLDER_ACCOUNT_INFO_LAYOUT = Struct(
     "tag" / Int8ul,

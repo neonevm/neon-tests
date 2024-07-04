63e9c5cbf47173ec3e346e21605f500373c38791
Andrei Zhidelev <169074588+andrei-neon@users.noreply.github.com>
2024-07-04 16:56:18 +0200

    add test for neon_getNativeTokenList

--- integration/tests/basic/rpc/test_neon_rpc_base_calls.py
+++ integration/tests/basic/rpc/test_neon_rpc_base_calls.py
@@ -110,3 +110,15 @@ class TestNeonRPCBaseCalls:
         )
         assert "error" not in response
         assert len(response["result"]) == 0, "expected empty result for non existent transaction request"
+
+    def test_neon_get_native_token_list(self, json_rpc_client):
+        response = json_rpc_client.send_rpc(method="neon_getNativeTokenList")
+        assert "error" not in response
+        assert len(response["result"]) > 1, f"Expected non-empty list, got {response['result']}"
+        for item in response["result"]:
+            assert "tokenChainId" in item
+            assert item["tokenChainId"] is not None
+            assert "tokenMint" in item
+            assert item["tokenMint"] is not None
+            assert "tokenName" in item
+            assert item["tokenName"] is not None

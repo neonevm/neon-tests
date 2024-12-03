dbbbe8e064f6f237e80795f4d6ec6769c87dadc3
romanova-natasha <zabelina.nattaly@gmail.com>
2024-12-03 04:01:09 +0000

    change eth_call according to the new data format
    change eth_call according to the new data format

--- loadtesting/k6/tests/tracer/ethCall.test.js
+++ loadtesting/k6/tests/tracer/ethCall.test.js
@@ -6,10 +6,10 @@ import { SharedArray } from 'k6/data';
 import exec from 'k6/execution';
 import { check } from 'k6';
 
-const ethGetStorageAtRequests = new Counter('eth_get_storage_at_requests');
-const ethGetStorageAtRequestErrorCounter = new Counter('eth_get_storage_at_request_errors');
-const ethGetStorageAtErrorCounter = new Counter('eth_get_storage_at_errors');
-const ethGetStorageAtRequestTime = new Trend('eth_get_storage_at_request_time', true);
+const ethCallRequests = new Counter('eth_get_storage_at_requests');
+const ethCallRequestErrorCounter = new Counter('eth_get_storage_at_request_errors');
+const ethCallErrorCounter = new Counter('eth_get_storage_at_errors');
+const ethCallRequestTime = new Trend('eth_get_storage_at_request_time', true);
 
 export const options = standardScenarioOptions;
 
@@ -25,10 +25,10 @@ const usersArray = new SharedArray('Users accounts', function () {
 });
 
 
-export default function EthGetStorageAtTest() {
+export default function EthCallTest() {
     const vuID = exec.vu.idInTest
     const index = vuID % usersArray.length;
-
+   
     const dataIndex = vuID % (historicalData.storage_contract_calls).length;
     const txInfo = historicalData.storage_contract_calls[dataIndex];
 
@@ -38,23 +38,23 @@ export default function EthGetStorageAtTest() {
     // blockNumber
     const requestParamsBlockNumber = {
         requestType: "blockNumber",
-        method: "eth_getStorageAt",
-        params: [txInfo.storage_contract_address, "0x0", {"blockNumber": txInfo.blockNumber}]
+        method: "eth_call",
+        params: [txInfo.retreive_function_tx, {"blockNumber": txInfo.blockNumber}]
     }
 
-    doRequest(client, requestParamsBlockNumber);
+    doRequest(client, requestParamsBlockNumber, txInfo.store_value);
 
     // blockHash
     const requestParamsBlockHash = {
         requestType: "blockHash",
-        method: "eth_getStorageAt",
-        params: [txInfo.storage_contract_address, "0x0", {"blockHash": txInfo.blockHash}]
+        method: "eth_call",
+        params: [txInfo.retreive_function_tx, {"blockHash": txInfo.blockHash}]
     }
     
-    doRequest(client, requestParamsBlockHash);
+    doRequest(client, requestParamsBlockHash, txInfo.store_value);
 }
 
-function doRequest(client, requestParams) {
+function doRequest(client, requestParams, expectedValue) {
     const startTime = new Date();
     try {
         const responseBody = client.callTracer(
@@ -63,17 +63,20 @@ function doRequest(client, requestParams) {
             JSON.stringify(requestParams.params)
         );
         const response = JSON.parse(responseBody);
+        console.log('response: ' + JSON.stringify(response));
+        console.log('expectedValue: ' + expectedValue);
+        console.log('result: ' + parseInt(response.result, 16));
         const checkResult = check(response, {
-            'response result is not 0': (r) => r.result != "0x0",
+            'response result is not expected value': (r) => parseInt(r.result, 16) == expectedValue,
         });
         if (!checkResult) {
-            console.log('Error in response of eth_getStorageAt: ' + JSON.stringify(response));
-            ethGetStorageAtRequestErrorCounter.add(1);
+            console.log('Error in response of eth_call: ' + JSON.stringify(response));
+            ethCallRequestErrorCounter.add(1);
         }
     } catch (e) {
-        console.log('Error in eth_getStorageAt: ' + e);
-        ethGetStorageAtErrorCounter.add(1);
+        console.log('Error in eth_call: ' + e);
+        ethCallErrorCounter.add(1);
     }
-    ethGetStorageAtRequestTime.add(new Date() - startTime);
-    ethGetStorageAtRequests.add(1);
+    ethCallRequestTime.add(new Date() - startTime);
+    ethCallRequests.add(1);
 }

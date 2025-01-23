4ba7d0b4099f24783d1b204f770429be36297def
IvanAdamovNeon <169074337+IvanAdamovNeon@users.noreply.github.com>
2025-01-23 05:51:39 -0800

    drop neon_revision validation (#489)

--- integration/tests/tracer/test_tracer_historical_methods.py
+++ integration/tests/tracer/test_tracer_historical_methods.py
@@ -359,7 +359,6 @@ class TestTracerHistoricalMethods:
         block = self.web3_client.get_block_number()
         revision = self.tracer_api.send_rpc(method="get_neon_revision", params=block)["result"]["neon_revision"]
         assert revision is not None
-        assert re.match(r"^v[\d]+\.[\d]+\.[\d]+$|^[a-fA-F\d]{40}$", revision) or revision == "latest"
 
     @pytest.mark.parametrize("block", [-190, '{"slot": 3f08}', "oneonetwozero", ["900"]])
     def test_neon_revision_invalid_block(self, block):

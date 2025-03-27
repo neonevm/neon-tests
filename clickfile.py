5ed5964d13ef346e9cdc9b365f47d13335117bb3
IvanAdamovNeon <169074337+IvanAdamovNeon@users.noreply.github.com>
2025-03-27 08:43:06 -0700

    moved cost report related code from clickfile.py (#573)
    * moved cost report related code from clickfile.py
    
    * fixed branch
    
    * revert!
    
    * fixed circular import

--- clickfile.py
+++ clickfile.py
@@ -14,19 +14,15 @@ from multiprocessing.dummy import Pool
 from pathlib import Path
 from urllib.parse import urlparse
 
-import pandas as pd
 import pytest
 
-from deploy.cli.cost_report import prepare_report_data, report_data_to_markdown
-from deploy.test_results_db.db_handler import PostgresTestResultsHandler
-from deploy.test_results_db.test_results_handler import TestResultsHandler
+from deploy.cli import cost_report
 from utils.accounts import EthAccounts
+from utils.consts import EnvName, TEST_GROUPS, EXTERNAL_CONTRACT_PATH
 from utils.error_log import error_log
 from utils.faucet import Faucet
 from utils.slack_notification import SlackNotification
 from utils.types import RepoType, TestGroup
-from utils.consts import EnvName, TEST_GROUPS, EXTERNAL_CONTRACT_PATH
-
 
 try:
     import click
@@ -393,44 +389,6 @@ def install_ui_requirements():
     # download the Playwright package and install browser binaries for Chromium, Firefox and WebKit.
     click.echo(green("Install browser binaries for Chromium."))
     subprocess.check_call("playwright install chromium", shell=True)
-
-
-def get_service_tags_for_cost_reports(
-    evm_tag: str,
-    proxy_tag: str,
-    repo: RepoType,
-    db: PostgresTestResultsHandler,
-    limit: int,
-    version_branch: str,
-) -> tuple[str, str, list[str]]:
-    """
-    :param evm_tag:
-    :param proxy_tag:
-    :param repo:
-    :param db:
-    :param limit: number of previous tags. E.g. if you want to compare 5 reports - you need 4 previous tags
-    :param version_branch:
-    :return:
-    """
-    compared_service_tag = evm_tag if repo == "evm" else proxy_tag
-    other_service_tag = evm_tag if repo == "proxy" else proxy_tag
-
-    # define the tags against which the comparison will be done
-    previous_tags: list[str]
-
-    if re.fullmatch(GITHUB_TAG_PATTERN, compared_service_tag):
-        previous_tags = db.get_previous_tags(
-            repo=repo,
-            tag=compared_service_tag,
-            limit=limit,
-        )
-    else:
-        if version_branch:
-            previous_tags = [version_branch]
-        else:
-            previous_tags = ["latest"]
-
-    return compared_service_tag, other_service_tag, previous_tags
 
 
 @click.group()
@@ -1161,58 +1119,21 @@ def save_dapps_cost_report_to_db(
     evm_commit_sha: str,
     proxy_commit_sha: str,
 ):
-    tag = evm_tag if repo == "evm" else proxy_tag
-
-    report_data = prepare_report_data(directory)
-    db = PostgresTestResultsHandler()
-
-    # define if previous similar reports should be deleted
-    is_neon_evm_tag_version_branch = bool(re.fullmatch(VERSION_BRANCH_TEMPLATE, evm_tag))
-    is_proxy_tag_version_branch = bool(re.fullmatch(VERSION_BRANCH_TEMPLATE, proxy_tag))
-
-    if evm_tag == proxy_tag == "latest":
-        click.echo("This is a merge to develop")
-        do_delete = False
-    elif is_neon_evm_tag_version_branch and is_proxy_tag_version_branch and evm_tag == proxy_tag:
-        click.echo(f"This is a merge to version branch {evm_tag}")
-        do_delete = False
-    else:
-        do_delete = True
-
-    # delete them if needed
-    if do_delete:
-        report_ids_old = db.get_cost_report_ids(repo=repo, tag=tag)
-        if report_ids_old:
-            db.delete_data_by_report_ids(report_ids=report_ids_old)
-            db.delete_reports(report_ids=report_ids_old)
-
-    # save the new report
-    report_id_new = db.save_cost_report(
+    cost_report.save_dapps_cost_report_to_db(
+        directory=directory,
         repo=repo,
-        neon_evm_tag=evm_tag,
+        evm_tag=evm_tag,
         proxy_tag=proxy_tag,
         evm_commit_sha=evm_commit_sha,
         proxy_commit_sha=proxy_commit_sha,
+        version_branch_template=VERSION_BRANCH_TEMPLATE,
     )
-    db.save_cost_report_data(report_data=report_data, cost_report_id=report_id_new)
 
 
 @dapps.command("save_dapps_cost_report_to_md", help="Save dApps Cost Report to cost_reports.md")
 @click.option("-d", "--directory", default="reports", help="Directory with reports")
 def save_dapps_cost_report_to_md(directory: str):
-    report_data = prepare_report_data(directory)
-
-    # Add 'gas_used_%' column after 'gas_used'
-    report_data.insert(
-        report_data.columns.get_loc("gas_used") + 1,
-        "gas_used_%",
-        (report_data["gas_used"] / report_data["gas_estimated"]) * 100,
-    )
-    report_data["gas_used_%"] = report_data["gas_used_%"].round(2)
-
-    # Dump report_data DataFrame to markdown, grouped by the dApp
-    report_as_markdown_table = report_data_to_markdown(df=report_data)
-    Path("cost_reports.md").write_text(report_as_markdown_table)
+    cost_report.save_dapps_cost_report_to_md(directory=directory)
 
 
 @dapps.command("compare_dapp_cost_reports", help="Compare dApp results")
@@ -1228,62 +1149,12 @@ def compare_dapp_results(
     version_branch: str,
     history_depth_limit: int,
 ):
-    """
-    >>> compared_service_tag
-    v1.1.1 - GitHub tag
-    feature_foo - feature branch
-
-    >>> other_service_tag
-    v1.1.x - version branch
-    feature_foo - feature branch
-    latest - develop branch
-    """
-    click.echo(f"compare_dapp_results: {locals()}")
-    db = PostgresTestResultsHandler()
-    compared_service_tag, other_service_tag, previous_tags = get_service_tags_for_cost_reports(
+    cost_report.compare_dapp_results(
+        repo=repo,
         evm_tag=evm_tag,
         proxy_tag=proxy_tag,
-        repo=repo,
-        db=db,
-        limit=history_depth_limit - 1,
         version_branch=version_branch,
-    )
-    click.echo(f"previous_tags: {previous_tags}")
-
-    historical_data = db.get_historical_data(
-        depth=history_depth_limit,
-        repo=repo,
-        latest_tag=compared_service_tag,
-        previous_tags=previous_tags,
-    )
-
-    # get commit sha for compared_service and other_service
-    data_sample_row = historical_data[
-        (historical_data["repo"] == repo)
-        & (historical_data["neon_evm_tag"] == evm_tag)
-        & (historical_data["proxy_tag"] == proxy_tag)
-    ].iloc[0]
-
-    if repo == "evm":
-        compared_service_commit_sha = data_sample_row["evm_commit_sha"]
-        other_service_commit_sha = data_sample_row["proxy_commit_sha"]
-    elif repo == "proxy":
-        compared_service_commit_sha = data_sample_row["proxy_commit_sha"]
-        other_service_commit_sha = data_sample_row["evm_commit_sha"]
-    else:
-        raise ValueError(f'Unknown repo "{repo}"')
-
-    compared_service_sha_string = f", commit sha {compared_service_commit_sha}" if compared_service_commit_sha else ""
-    other_service_sha_string = f", commit sha {other_service_commit_sha}" if other_service_commit_sha else ""
-
-    # generate plots and save to pdf
-    other_service_name = "neon_evm" if repo == "proxy" else "proxy"
-    test_results_handler = TestResultsHandler()
-    test_results_handler.generate_and_save_plots_pdf(
-        historical_data=historical_data,
-        title_end=f"on {repo}:{compared_service_tag}{compared_service_sha_string}\n"
-        f"with {other_service_name}:{other_service_tag}{other_service_sha_string}",
-        output_pdf="cost_reports.pdf",
+        history_depth_limit=history_depth_limit,
     )
 
 
@@ -1310,78 +1181,18 @@ def validate_cost_reports(
     compute_units: int,
     output: str,
 ):
-    """
-    Compares the cost report data for <repo>:<evm_tag|proxy_tag|version_branch>
-    with previous report data based on acceptable absolute increases in metrics.
-    Any detected increases exceeding the allowed thresholds are saved to the <output> file.
-
-    :param repo: Repository name.
-    :param evm_tag: EVM tag of the report data.
-    :param proxy_tag: Proxy tag of the report data.
-    :param version_branch: Maximum acceptable absolute increase in the metric version_branch.
-    :param acc_count: Maximum acceptable absolute increase in the metric acc_count.
-    :param trx_count: Maximum acceptable absolute increase in the metric trx_count.
-    :param gas_estimated: Maximum acceptable absolute increase in the metric gas_estimated.
-    :param gas_used: Maximum acceptable absolute increase in the metric gas_used.
-    :param compute_units: Maximum acceptable absolute increase in the metric compute_units.
-    :param output: Path to the JSON file where detected failures are saved.
-    """
-    db = PostgresTestResultsHandler()
-    compared_service_tag, other_service_tag, previous_tags = get_service_tags_for_cost_reports(
+    cost_report.validate_cost_reports(
+        repo=repo,
         evm_tag=evm_tag,
         proxy_tag=proxy_tag,
-        repo=repo,
-        db=db,
-        limit=1,
         version_branch=version_branch,
+        acc_count=acc_count,
+        trx_count=trx_count,
+        gas_estimated=gas_estimated,
+        gas_used=gas_used,
+        compute_units=compute_units,
+        output=output,
     )
-    click.echo(f"previous_tags: {previous_tags}")
-
-    historical_data = db.get_historical_data(
-        depth=2,
-        repo=repo,
-        latest_tag=compared_service_tag,
-        previous_tags=previous_tags,
-    )
-
-    all_metric_names = "acc_count", "trx_count", "gas_estimated", "gas_used", "compute_units"
-    dapp_names = historical_data["dapp_name"].unique()
-
-    failure = tp.TypedDict("failure", {"dapp": str, "action": str, "metric": str, "increase": int})
-    failures: list[failure] = []
-
-    for dapp_name in dapp_names:
-        data_for_dapp = historical_data[historical_data["dapp_name"] == dapp_name]
-        actions = data_for_dapp["action"].unique()
-
-        for action in actions:
-            data_for_dapp_action = data_for_dapp[data_for_dapp["action"] == action]
-            metric_names = [col_name for col_name in data_for_dapp_action.columns if col_name in all_metric_names]
-
-            for metric_name in metric_names:
-                metric_values = data_for_dapp_action[metric_name]
-                historical_value = metric_values.iloc[0]
-                latest_value = metric_values.iloc[-1]
-
-                if not pd.isna(historical_value) and not pd.isna(latest_value):
-                    actual_change = latest_value - historical_value
-                    max_acceptable_change = locals()[metric_name]
-
-                    if actual_change > max_acceptable_change:
-                        failure_dict: failure = {
-                            "dapp": dapp_name,
-                            "action": action,
-                            "metric": metric_name,
-                            "increase": actual_change,
-                        }
-                        failures.append(failure_dict)
-
-    if failures:
-        df = pd.DataFrame(failures)
-        md = df.to_markdown(index=False)
-
-        with open(output, "w") as f:
-            json.dump(md, f)
 
 
 @dapps.command("add_pr_comment", help="Add PR comment with dApp cost reports")

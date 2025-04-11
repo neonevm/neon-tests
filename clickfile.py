d8236991d3346d7c4c82b8bf58e6bb31db4129b5
Kristina Nikolaeva <112946046+kristinaNikolaevaa@users.noreply.github.com>
2025-04-11 15:18:25 +0200

    Ndev 3683 retry devnet tests (#612)

--- clickfile.py
+++ clickfile.py
@@ -560,6 +560,8 @@ def run(
             )
         if numprocesses:
             command = f"{command} --numprocesses {numprocesses} --dist loadgroup"
+        if network == network.DEVNET:
+            command += " --retries 3 --retry-delay 2"
 
     elif name == "basic_extended":
         # run basic excluding tests for ERC20SPLNew contract

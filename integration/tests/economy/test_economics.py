b60260c6ca052c4b66a1ce03f4b3195959a3a478
Natalia Romanova <nataliaromanova@Natalias-MacBook-Air.local>
2025-05-08 12:25:05 +0100

    fix gas value
    fix gas value

--- integration/tests/economy/test_economics.py
+++ integration/tests/economy/test_economics.py
@@ -1108,14 +1108,11 @@ class TestEconomics:
         instruction_tx = counter_contract.functions.moreInstruction(0, 3000).build_transaction(tx)
         receipt = web3_client.send_transaction(accounts[0], instruction_tx)
         assert receipt["status"] == 1
-
         gas_used = receipt["gasUsed"]
-        print("Gas used: ", gas_used)
 
-        tx = web3_client.make_raw_tx(from_=accounts[0].address, tx_type=TransactionType.EIP_1559, gas=gas_used // 100)
+        tx = web3_client.make_raw_tx(from_=accounts[0].address, tx_type=TransactionType.EIP_1559, gas=gas_used // 23)
         instruction_tx = counter_contract.functions.moreInstruction(0, 3000).build_transaction(tx)
         receipt = web3_client.send_transaction(accounts[0], instruction_tx)
-        print("Failed tx receipt: ", receipt)
         assert receipt["status"] == 0
 
         sol_balance_after = operator.get_solana_balance()

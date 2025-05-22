24115259fefa463ff7fca09416e5e733921ca76d
Natalia Romanova <nataliaromanova@Natalias-MacBook-Air.local>
2025-05-22 15:33:07 +0100

    fix revision contract method
    fix revision contract method

--- contracts/common/Revision.sol
+++ contracts/common/Revision.sol
@@ -6,38 +6,6 @@ contract RevisionChanger {
     bytes32[64] public b;
     uint256 public number_outer_contract_scope = 2;
 
-    function powNumberInnerAndRollback(uint256 n) public {
-        require(n > 0, "Exponent should be > 0");
-
-        uint original = number_inner_contract_scope;
-        uint computedValue = 1;
-
-        for (uint i = 0; i < n; i++) {
-            computedValue *= original;
-            number_inner_contract_scope = computedValue;
-        }
-        number_inner_contract_scope = original;
-    }
-
-    function powNumberOuterAndRollback(uint256 n) public {
-        require(n > 0, "Exponent should be > 0");
-
-        uint original = number_outer_contract_scope;
-        uint computedValue = 1;
-
-        for (uint i = 0; i < n; i++) {
-            computedValue *= original;
-            number_outer_contract_scope = computedValue;
-        }
-    }
-
-}
-
-contract RevisionChangerForCaller {
-    uint256 public number_inner_contract_scope = 1;
-    bytes32[64] public b;
-    uint256 public number_outer_contract_scope = 2;
-
     function getVarB() public view returns (bytes32[64] memory) {
         return b;
     }
@@ -65,6 +33,7 @@ contract RevisionChangerForCaller {
             computedValue *= original;
             number_outer_contract_scope = computedValue;
         }
+        number_outer_contract_scope = original;
     }
 
     function changeGlobalVarB(uint256 n) public {
@@ -75,9 +44,9 @@ contract RevisionChangerForCaller {
 }
 
 contract RevisionChangerCaller {
-    RevisionChangerForCaller rch;
-    constructor(address revisionChangerForCallerAddress) {
-        rch = RevisionChangerForCaller(revisionChangerForCallerAddress);
+    RevisionChanger rch;
+    constructor(address revisionChangerAddress) {
+        rch = RevisionChanger(revisionChangerAddress);
     }
 
     function callRevisionChangerMethods(uint256 n) public {

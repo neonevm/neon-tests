7d7442208ca00c7aa5aa1f3da87e09b3346f268d
sshiganov <stanislav.shiganov@gmail.com>
2025-06-23 14:46:29 +0300

    fixed issues

--- integration/tests/neon_evm/solana_native/test_scheduled_trx_balance.py
+++ integration/tests/neon_evm/solana_native/test_scheduled_trx_balance.py
@@ -7,7 +7,7 @@ from utils.scheduled_trx import ScheduledTrxEstimateRequest
 
 
 def test_iter_successful_trx_balance_with_outer_deposit(
-    neon_user, evm_loader, operator_keypair, web3_client_sol, treasury_pool, common_contract, solana_account
+    neon_user, evm_loader, operator_keypair, treasury_pool, basic_contract, solana_account, neon_api_client
 ):
     # trx: non-iterable (1 solana trx in chain)
     # trx_status: successful
@@ -20,27 +20,37 @@ def test_iter_successful_trx_balance_with_outer_deposit(
     holder_acc_balance = evm_loader.get_solana_balance(holder_acc)
     operator_balance = evm_loader.get_solana_balance(operator_keypair.pubkey())
     neon_user_balance_before = evm_loader.get_solana_balance(neon_user.solana_account.pubkey())
+    # neon_user_balance_before_sol = web3_client_sol.get_balance(neon_user.checksum_address)
     treasury_pool_balance = evm_loader.get_solana_balance(treasury_pool.account)
 
     print(f"{neon_user_balance_before=}")
+    # print(f"{neon_user_balance_before_sol=}")
     print(f"{operator_balance=}")
     print(f"{treasury_pool_balance=}")
     print(f"{holder_acc_balance=}")
 
-    nonce = web3_client_sol.get_nonce(neon_user.checksum_address)
+    # nonce = web3_client_sol.get_nonce(neon_user.checksum_address)
+    nonce = evm_loader.get_neon_nonce(neon_user.neon_address, evm_loader.sol_chain_id)
     call_data = decode_function_signature("setNumber(uint256)", args=[10])
+    # neon_api_client.simulate_solana()
 
-    trx_estimate_obj = ScheduledTrxEstimateRequest(neon_user.checksum_address, common_contract.address, call_data)
-    estimate_result = web3_client_sol.estimate_scheduled(neon_user.solana_account.pubkey(), [trx_estimate_obj])
+    # estimate_result = (neon_user.solana_account.pubkey(), [trx_estimate_obj])
 
-    tx0 = ScheduledTransaction.from_estimate_result(0, trx_estimate_obj, estimate_result)
+    # tx0 = ScheduledTransaction.from_estimate_result(0, trx_estimate_obj, estimate_result)
 
-    tree_acc_data = CreateTreeAccMultipleData(
-        nonce=nonce,
-        max_fee_per_gas=estimate_result["maxFeePerGas"],
-        max_priority_fee_per_gas=estimate_result["maxPriorityFeePerGas"],
+    tx = ScheduledTransaction(
+        neon_user.neon_address,
+        None,
+        nonce,
+        0,
+        target=basic_contract.eth_address,
+        value=0,
+        call_data=call_data,
+        chain_id=evm_loader.sol_chain_id,
     )
-    tree_acc_data.add_trx(tx0, 0xFFFF, 0)
+
+    tree_acc_data = CreateTreeAccMultipleData(nonce=nonce)
+    tree_acc_data.add_trx(tx, 0xFFFF, 0)
     tree_acc = evm_loader.create_tree_account_multiple(neon_user, treasury_pool, tree_acc_data.data)
 
     print("\n----Balances after tree acc was created----")
@@ -52,20 +62,20 @@ def test_iter_successful_trx_balance_with_outer_deposit(
     treasury_pool_balance_after_tree = evm_loader.get_solana_balance(treasury_pool.account)
     tree_acc_balance = evm_loader.get_solana_balance(tree_acc)
 
-    print(f"Delta neon_user {neon_user_balance_after_tree}")
-    print(f"Delta operator {operator_balance_after_tree}")
-    print(f"Delta holder {holder_acc_balance_after_tree}")
-    print(f"Delta treasury {treasury_pool_balance_after_tree}")
+    print(f"Neon_user {neon_user_balance_after_tree}")
+    print(f"Operator {operator_balance_after_tree}")
+    print(f"Holder {holder_acc_balance_after_tree}")
+    print(f"Treasury {treasury_pool_balance_after_tree}")
     print(f"Tree Acc balance {tree_acc_balance}")
 
-    evm_loader.write_transaction_to_holder_account(tx0.encode(), holder_acc, operator_keypair)
+    evm_loader.write_transaction_to_holder_account(tx.encode(), holder_acc, operator_keypair)
     additional_accounts = [
-        Pubkey.from_string(evm_loader.ether2program(common_contract.address)[0]),  # solana_address
+        basic_contract.solana_address,  # solana_address
         neon_user.get_balance_account(evm_loader.sol_chain_id),
     ]
 
     evm_loader.execute_scheduled_trx_from_instruction_with_details(
-        tx0, operator_keypair, holder_acc, tree_acc, treasury_pool, additional_accounts
+        tx, operator_keypair, holder_acc, tree_acc, treasury_pool, additional_accounts
     )
 
     evm_loader.finish_scheduled_trx(operator_keypair, tree_acc, holder_acc)

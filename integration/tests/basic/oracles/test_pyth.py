96bd6cf3bb9f530b769e53323a39bf0f9b1a72b4
Natalia Romanova <nataliaromanova@Natalias-MacBook-Air.local>
2025-06-26 11:47:00 +0100

    rm mainnet prices check
    rm mainnet prices check

--- integration/tests/basic/oracles/test_pyth.py
+++ integration/tests/basic/oracles/test_pyth.py
@@ -5,21 +5,6 @@ from utils.web3client import NeonChainWeb3Client
 from utils.consts import REMAPPING_ZEPPELIN
 
 
-PYTH_MAINNET = {
-    "SOL/USDC": "0x66d23fc4521d75613921f6475ce1776ed4a8f109",
-    "USDC/USD": "0x94CDaE0758F7dA5EcA97646A665345BC20f72D53",
-    "USDT/USD": "0xb22f95f4F203646ffe5752A5C1142A359c82cD47",
-    "ETH/USD": "0x9ca5Ae7Fdc9Ef33fd8B86634678252Af052cF920",
-    "NEON/USD": "0x5418Bd0bd3A43D6DcC486fb374a2346BE5e07A0D",
-    "BTC/USD": "0x4359E879c83fB21e33BB62061bf22806873F06d6",
-    "WBTC/USD": "0x8C96809746B45e1506007613d6Ec035cA41bEcB4",
-    "JITOSOL/USD": "0x05CE377b7df379460EdEcB2baa3Ca18fB59b082C",
-    "MSOL/USD": "0x47b1aD7a08D026a54DEd3d9C3935173FEdfbD2CF",
-    "BONK/USD": "0x3d22FD7e59D19e08a6D5f55aD720549339fc8544",
-    "JUP/USD": "0xD98d90B922C0a7112825232C7380B99176F090A7",
-    "INF/USD": "0x4Ff8DfecEb1d29bbF58e92Cc9847fd20b51406aD",
-}
-
 PYTH_DEVNET = {
     "SOL/USDC": "0x19c6315fCb69aAE8eB74f0c8a6c1a1DD9540F64f",
     "USDC/USD": "0xdc339bBBFfab4ED48F387e2247f5e2a19EFD33D1",
@@ -51,18 +36,11 @@ class TestPyth:
         )
         return contract
 
-    def get_prices(self, pyth_addresses):
-        for pair, address in pyth_addresses.items():
+    @pytest.mark.only_devnet
+    def test_get_pyth_prices_devnet(self):
+        for pair, address in PYTH_DEVNET.items():
             contract = self.get_pyth_contract(address)
             price = contract.functions.latestAnswer().call()
             assert price is not None
             with allure.step(f"Pyth price for pair {pair} is {price}"):
                 pass
-
-    @pytest.mark.only_devnet
-    def test_get_pyth_prices_devnet(self):
-        self.get_prices(PYTH_DEVNET)
-
-    @pytest.mark.mainnet
-    def test_get_pyth_prices_mainnet(self):
-        self.get_prices(PYTH_MAINNET)
